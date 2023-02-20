b47218268f5cec0c4e02c080e869b5bf0f1c4901
heryoo <heryoo@naver.com>
2023-02-20 23:46:32 +0900

    [정렬, 맵, 셋] 0220

---
+++ 01_정렬, 맵, 셋/필수/14425.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int n,m; 
+    int count = 0; //집합에서 찾은 문자열 개수
+    cin >> n >> m ; 
+    map<string, int> string_map;
+
+    for(int i=0; i<n; i++){
+        string str;
+        cin >> str;
+        string_map[str] = 1;
+    }
+
+    for(int i=0; i<m; i++){
+        string search_str;
+        cin >> search_str;
+        if(string_map[search_str] == 1){
+            count += 1 ;
+        }
+    }
+    cout << count << '\n';
+
+    return 0;
+}
---
+++ 01_정렬, 맵, 셋/필수/19636.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int weightChange(int weight, int energy_plus, int energy_base, int energy_act){
+    weight += energy_plus - (energy_base + energy_act);
+    return weight;
+}
+
+void diet1(int weight, int energy_plus, int energy_base, int energy_act, int day){
+    for(int i=0; i<day; i++){
+        weight = weightChange(weight, energy_plus, energy_base, energy_act);
+        if ((weight<=0)||(energy_base<=0)){
+            cout << "Danger Diet \n";
+            return;
+        }
+    }
+    cout << weight << ' ' << energy_base << '\n';
+}
+
+void diet2(int weight, int energy_plus, int energy_base, int energy_act, int day, int energy_diff){
+    int base = energy_base;
+    for(int i=0; i<day; i++){
+        weight = weightChange(weight, energy_plus, energy_base, energy_act);
+        if ((energy_plus - (energy_base + energy_act))<(-1)*energy_diff){
+            energy_base += (energy_plus - (energy_base + energy_act))/2 ; 
+        }
+        if ((weight<=0)||(energy_base<=0)){
+            cout << "Danger Diet \n";
+            return;
+        }
+    }
+
+    if ((base-energy_base)>0){
+        cout <<  weight <<' '<< energy_base <<' '<< "YOYO" << '\n';
+    }
+    else {
+        cout <<  weight <<' '<< energy_base <<' '<< "NO" << '\n';
+    }
+}
+
+int main(){
+    int weight, energy_plus, energy_base, energy_act;
+    int energy_diff;
+    int day;
+
+    cin >> weight >> energy_base >> energy_diff;
+    cin >> day >> energy_plus >> energy_act;
+
+    diet1(weight, energy_plus, energy_base, energy_act, day);
+    diet2(weight, energy_plus, energy_base, energy_act, day, energy_diff);
+    return 0;
+
+
+
+
+
+}
