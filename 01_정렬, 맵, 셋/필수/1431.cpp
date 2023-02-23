fb588c2be13035b345cbcb697669772ea04a9c14
doh-ha <hayeonlr@gmail.com>
2023-02-23 17:22:38 +0900

    [정렬, 맵, 셋] 2월 23일

--- 01_정렬, 맵, 셋/필수/1431.cpp
+++ 01_정렬, 맵, 셋/필수/1431.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-bool compare
-
 int sumNum(string serial_num) {
 	int sum = 0;
 	for (int i = 0; i < serial_num.size(); i++) {
@@ -17,6 +15,23 @@ int sumNum(string serial_num) {
 	return sum;
 }
 
+bool compare (const string& s1, const string& s2)
+{
+  if(s1.length() != s2.length()){ //1) 길이 다르면 짧은 거 먼저
+    return s1.length() < s2.length();
+  }
+
+  else{
+    if (sumNum(s1) != sumNum(s2)) { //2) 자리수의 합 작은 거 먼저
+		return sumNum(s1) < sumNum(s2);
+    }
+    else return s1 < s2; //3) 알파벳 사전 순
+  }
+
+}
+
+
+
 int main()
 {
 	int n; //기타의 개수
@@ -25,7 +40,7 @@ int main()
 
 	// 입력
 	cin >> n;
-  	vector<string> guitar(n, ""); //시리얼 번호
+  	vector<string> input(n, ""); //시리얼 번호
   for (int i=0;i<n;i++){
     cin >> input[i];
   }
