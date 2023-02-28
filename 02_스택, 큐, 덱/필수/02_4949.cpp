866a6ca8decdd10e882ca0a728eea19ade71a074
JisooJeong <113654733+JisooJudy@users.noreply.github.com>
2023-02-28 18:06:12 +0900

    [스택, 큐, 덱] 0228

---
+++ 02_스택, 큐, 덱/필수/02_4949.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    stack<char> stack;
+
+    while(1) {
+
+        string s;
+        getline(cin, s);
+
+        if(s=="."){
+            break;
+        }
+
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '[') {
+                stack.push(s[i]);
+            }
+            else if (s[i] == ')') {
+                if (!stack.empty() && stack.top() == '(') {
+                    stack.pop();
+                }
+                else {
+                    cout << "no\n";
+                    break;
+                }
+            }
+            else if (s[i] == ']') {
+                if (!stack.empty() && stack.top() == '[') {
+                    stack.pop();
+                }
+                else {
+                    cout << "no\n";
+                    break;
+                }
+            }
+            else {
+                cout << "no\n";
+                break;
+            }
+
+        }
+        if (stack.empty()) {
+            cout << "yes\n";
+        }
+        else {
+            cout << "no\n";
+        }
+    }
+    return 0;
+}
