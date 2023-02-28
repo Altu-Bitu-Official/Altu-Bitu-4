3b869381c7042cfcbc2f7b0b1e660eb4823ef059
aqswa <joayoung3@gmail.com>
2023-03-01 02:49:16 +0900

    [Stack, Queue, Deque] 0228

--- 02_스택, 큐, 덱/필수/10757.cpp
+++ 02_스택, 큐, 덱/필수/10757.cpp
@@ -1,4 +1,59 @@
-//
-// Created by LG on 2023-02-28.
-//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    string a;
+    string b;
+
+    cin >> a >> b;
+
+    if(a.length() < b.length()){
+        string temp = a;
+        a = b;
+        b = temp;
+    }
+
+    int a_len = a.length();
+    int b_len = b.length();
+    int sum = 0;
+    int carry = 0;
+    vector<int> res(a_len+1);
+
+    while(b_len > 0){
+        sum = a[a_len-1]-'0' + b[b_len-1]-'0' + carry;
+
+        if(sum > 9){
+            sum-= 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+        res[a_len] = sum;
+        a_len--;
+        b_len--;
+    }
+
+    while(a_len>0){
+        sum = a[a_len-1]-'0' + carry;
+        if(sum>9){
+            sum -= 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+        res[a_len] = sum;
+        a_len--;
+    }
+
+    if(carry !=0) {
+        res[0] = carry;
+        for (int i = 0; i <= a.length(); i++)
+            cout << res[i];
+    }
+    else{
+        for(int i=1; i<=a.length(); i++)
+            cout << res[i];
+    }
+}
 
--- 02_스택, 큐, 덱/필수/4949.cpp
+++ 02_스택, 큐, 덱/필수/4949.cpp
@@ -1,4 +1,44 @@
-//
-// Created by LG on 2023-02-28.
-//
+#include <iostream>
+#include <vector>
+#include <stack>
 
+using namespace std;
+
+bool check(string s){
+    stack<char> st;
+    for(int i=0; i<s.size(); i++){
+        if(s[i] == '(' || s[i] == '[')
+            st.push(s[i]);
+        else if(s[i] == ')'){
+            if(!st.empty() && st.top() == '(')
+                st.pop();
+            else
+                return false;
+        }
+        else if(s[i] == ']'){
+            if(!st.empty() && st.top() == '[')
+                st.pop();
+            else
+                return false;
+        }
+    }
+    if(!st.empty())
+        return false;
+    return true;
+}
+
+int main(){
+
+    string s;
+    cin.ignore();
+    getline(cin, s);
+
+    while(s != "."){
+        if(check(s))
+            cout << "yes";
+        else
+            cout << "no";
+        cout << '\n';
+        getline(cin, s);
+    }
+}
