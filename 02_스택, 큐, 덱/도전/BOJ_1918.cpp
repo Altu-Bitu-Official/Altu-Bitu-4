14e63169a85b5ffe83aed3cdb3969def46aaf741
kimhj010502 <kimhj010502@ewhain.net>
2023-02-28 18:22:24 +0900

    [스택, 큐, 덱] 0228

---
+++ 02_스택, 큐, 덱/도전/BOJ_1918.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cctype>
+
+using namespace std;
+
+int rankNum(const char& c) {
+	if (c == '*' || c == '/') {
+		return 2;
+	}
+	else if (c == '+' || c == '-') {
+		return 1;
+	}
+	else if (c == '(') {
+		return 0;
+	}
+}
+
+void postfix(string input, stack<char>& s, queue<char>& q) {
+	for (int i = 0; i < input.length(); i++) {
+		if (isalpha(input[i])) { //알파벳인 경우
+			q.push(input[i]);
+		}
+
+		else if (input[i] == '(') { //'('는 무조건 삽입
+			s.push(input[i]);
+		}
+		else if (input[i] == ')') {//'('가 나올 때까지 스택에 있는 연산자 큐에 옮기기
+			while (s.top() != '(') {
+				q.push(s.top());
+				s.pop();
+			}
+			s.pop(); //'(' 제거
+		}
+
+		else { //연산자인 경우
+			if (s.empty()) { //스택이 비어있으면 삽입
+				s.push(input[i]);
+			}
+			else { //스택이 비어있지 않은 경우
+				while (!s.empty()) { //우선순위 고려해서 스택에 있는 연산자 큐에 옮기기
+					if (rankNum(input[i]) <= rankNum(s.top())) { //우선순위 비교
+						q.push(s.top());
+						s.pop();
+					}
+					else {
+						break;
+					}
+				}
+				s.push(input[i]); //현재 연산자 스택에 삽입
+			}
+		}
+	}
+
+	while (!s.empty()) { //스택에 남은 연산자 큐에 삽입
+		q.push(s.top());
+		s.pop();
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	string input;
+	getline(cin, input);
+
+	//연산
+	stack<char> s;
+	queue<char> q;
+	postfix(input, s, q);
+	
+	//출력
+	while(!q.empty()) {
+		cout << q.front();
+		q.pop();
+	}
+
+	return 0;
+}
---
+++ 02_스택, 큐, 덱/필수/BOJ_4949.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string check(string& input, stack<char>& s) {
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] == '(' || input[i] == '[') { //왼쪽 괄호는 stack에 넣기
+			s.push(input[i]);
+		}
+		else if (input[i] == ')') { //오른쪽 괄호인 경우
+			if (s.empty()) { //stack이 비어있지 검사
+				return "no";
+			}
+			else {
+				if (s.top() == '(') { //가장 마지막의 왼쪽 괄호랑 짝이 맞는지 비교
+					s.pop(); //맞다면 왼쪽 괄호도 같이 없애기
+				}
+				else {
+					return "no";
+				}
+			}
+		}
+		else if (input[i] == ']') {
+			if (s.empty()) {
+				return "no";
+			}
+			else {
+				if (s.top() == '[') {
+					s.pop();
+				}
+				else {
+					return "no";
+				}
+			}
+		}
+	}
+
+	if (s.empty()) { //모든 문자를 검사하고 stack이 비어있다면 균형을 이룸
+		return "yes";
+	}
+	else {
+		return "no";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string input;
+	
+	while (true) {
+		//입력
+		getline(cin, input);
+		stack<char> s;
+		if (input == ".") {
+			break;
+		}
+		//연산 & 출력
+		cout << check(input, s) << "\n";
+	}
+	
+	return 0;
+}
