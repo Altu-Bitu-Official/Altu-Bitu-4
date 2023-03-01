5a1c90c52ab36485f24bf9e2beeb3f22c77f38cc
cakenpeace <chwon02@naver.com>
2023-03-01 23:53:51 +0900

    [스택, 큐, 덱] 0301 - Update

--- 02_스택, 큐, 덱/필수/BOJ_1158.cpp
+++ 02_스택, 큐, 덱/필수/BOJ_1158.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// 
+// 요세푸스 순열을 리턴하는 함수
 queue<int> josep(static queue<int>& q, int n, int k) {
     queue<int> answer;
     for (int i = 0; i < n; i++) { // i번째 사람 -> i-1번 앞사람 뒤로 보내기
@@ -28,6 +28,7 @@ int main() {
     
     answer = josep(q, n, k);
     
+    // 정답 출력
     cout << '<';
 
     for (int i = 0; i < n; i++) {
