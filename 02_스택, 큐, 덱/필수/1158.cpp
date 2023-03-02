65daaf72830bde2941c2d6f651dbd6fa6c237c2e
doh-ha <hayeonlr@gmail.com>
2023-03-02 22:23:39 +0900

    [스택, 큐, 덱] 3월 2일 -p1 수정 ing
    <3, 6, 2, 7> 까지

--- 02_스택, 큐, 덱/필수/1158.cpp
+++ 02_스택, 큐, 덱/필수/1158.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 int n, k;  
-queue<int> q; 
+queue<int> q1; //입력 큐
+queue<int> q2; //결과 큐
 
 int main(){
      ios::sync_with_stdio(false);
@@ -16,27 +17,36 @@ int main(){
 cin >> n >> k;
 
 for (int i=1; i<=n; i++){
-  q.push(i);
+  q1.push(i);
 }
 
 cout << "<";
 
 //출력
-while(!q.empty()){
+while(!q1.empty()){
+  int tmp;
  for(int i = 0; i < k-1; i++){
-			if(i!=k){ //k번쨰 숫자가 아니면 앞에서 빼서 뒤로 다시 넣기
-        q.pop();
-        q.push(i);
+  tmp=q1.front();
 
-      }
-    else{
-      cout << q.front() << ", ";
-      q.pop();
-      
+        q1.pop(); //앞으로 빼서 다시 뒤로 넣기
+        q1.push(tmp);
+ }
+    //k번째이면 결과 큐에 삽입
+    tmp=q1.front();
 
-    }
+        q1.pop(); 
+        q2.push(tmp);
+
+ 
+
+
+
 		}
-	}
+    
+    for(int i=0; i<q2.size(); i++){
+       cout << q2.front() << ", ";
+       q2.pop();
+    }
 	cout<<">";	
 
 return 0;
