e8b5e7e197091d7c5b10625ffc7b596b8fc7ef00
kimhj010502 <kimhj010502@ewhain.net>
2023-03-02 22:23:33 +0900

    [스택, 큐, 덱] 0302

--- 02_스택, 큐, 덱/필수/BOJ_1158.cpp
+++ 02_스택, 큐, 덱/필수/BOJ_1158.cpp
@@ -4,25 +4,30 @@
 
 using namespace std;
 
-void josephus(vector<int>& num, queue<int>& result, const int& n, const int& k) {
-	for (int i = 0; i < n; i++) {
-		num[i] = i + 1;
+vector<int> josephus(const int& n, const int& k) {
+	queue<int> num;
+	vector<int> result;
+
+	for (int i = 1; i <= n; i++) {
+		num.push(i);
 	}
 
-	int index = -1;
+	int index = 0;
 	while (result.size() != n) { //result의 총 개수가 n이 될 때까지 반복
-		int t = 1; //t: 다음 k번째에 숫자가 있는 곳까지 이동해야 할 거리
-		int check = 0; //check: k번째까지 갔는지 확인하기 위한 변수
-		while (check < k) {
-			if (num[(index + t) % n] != 0) { //없어진 숫자가 아니라면 check 1 증가
-				check++;
-			}
-			t++; //이동 거리 증가
+		int t = num.front(); //큐의 front 사람
+		if (index < k - 1) { //해당 사람이 차례가 아닌 경우
+			num.push(t); //다시 큐에 넣기
+			num.pop(); //front에 있는 사람 삭제
+			index++; //index 증가
 		}
-		index = (index + t - 1) % n; //다음에 없애야 하는 숫자의 index
-		result.push(num[index]); //해당 숫자 result 큐에 넣기
-		num[index] = 0; //벡터에서 해당 숫자를 0으로 바꿔줌으로써 숫자 삭제
+		else { //해당 사람이 차례인 경우
+			index = 0; //index 초기화
+			result.push_back(t); //결과 벡터에 넣기
+			num.pop();
+		}
 	}
+
+	return result;
 }
 
 int main() {
@@ -34,17 +39,14 @@ int main() {
 	cin >> n >> k;
 
 	//연산
-	vector<int> num(n); //1부터 n까지의 숫자가 있는 벡터 생성
-	queue<int> result; //최종 결과를 저장할 큐 생성
-	josephus(num, result, n, k);
-	
+	vector<int> result = josephus(n, k);
+
 	//출력
 	cout << "<";
-	for (int i = 0; i < n - 1; i++) { //큐에 저장된 숫자 순서대로 하나씩 출력
-		cout << result.front() << ", ";
-		result.pop();
+	for (int i = 0; i < n - 1; i++) { //결과 벡터에 저장된 숫자 순서대로 하나씩 출력
+		cout << result[i] << ", ";
 	}
-	cout << result.front() << ">";
-	
+	cout << result[n - 1] << ">";
+
 	return 0;
 }
--- 02_스택, 큐, 덱/필수/BOJ_4949.cpp
+++ 02_스택, 큐, 덱/필수/BOJ_4949.cpp
@@ -6,43 +6,34 @@ using namespace std;
 
 string check(string& input, stack<char>& s) {
 	for (int i = 0; i < input.length(); i++) {
-		if (input[i] == '(' || input[i] == '[') { //왼쪽 괄호는 stack에 넣기
-			s.push(input[i]);
-		}
-		else if (input[i] == ')') { //오른쪽 괄호인 경우
-			if (s.empty()) { //stack이 비어있지 검사
-				return "no";
-			}
-			else {
-				if (s.top() == '(') { //가장 마지막의 왼쪽 괄호랑 짝이 맞는지 비교
-					s.pop(); //맞다면 왼쪽 괄호도 같이 없애기
-				}
-				else {
-					return "no";
-				}
-			}
-		}
-		else if (input[i] == ']') {
-			if (s.empty()) {
-				return "no";
-			}
-			else {
-				if (s.top() == '[') {
+		char w = input[i];
+		switch (w) {
+			case '(': case '['://왼쪽 괄호는 stack에 넣기
+				s.push(w);
+				break;
+			case ')':
+				if (!s.empty() && s.top() == '(') {
 					s.pop();
 				}
 				else {
 					return "no";
 				}
-			}
+				break;
+			case ']':
+				if (!s.empty() && s.top() == '[') {
+					s.pop();
+				}
+				else {
+					return "no";
+				}
+				break;
 		}
 	}
 
 	if (s.empty()) { //모든 문자를 검사하고 stack이 비어있다면 균형을 이룸
 		return "yes";
 	}
-	else {
-		return "no";
-	}
+	return "no";
 }
 
 int main() {
@@ -50,7 +41,7 @@ int main() {
 	cin.tie(NULL); cout.tie(NULL);
 
 	string input;
-	
+
 	while (true) {
 		//입력
 		getline(cin, input);
@@ -58,9 +49,10 @@ int main() {
 		if (input == ".") {
 			break;
 		}
+
 		//연산 & 출력
 		cout << check(input, s) << "\n";
 	}
-	
+
 	return 0;
 }
