a95f885996bd386b480f1160501ea05a7478f94f
doh-ha <hayeonlr@gmail.com>
2023-03-02 23:40:16 +0900

    [스택, 큐, 덱] 3월 2일 -Update
    1158 맞았습니다.

--- 02_스택, 큐, 덱/필수/1158.cpp
+++ 02_스택, 큐, 덱/필수/1158.cpp
@@ -23,41 +23,35 @@ int main()
 
   cout << "<";
 
-  // 출력
-  while (1)
+  // 결과 큐에 입력
+  while (!q1.empty())
   {
     int tmp;
     for (int i = 0; i < k - 1; i++)
     {
-      tmp = q1.front();
+      q1.push(q1.front());
       q1.pop(); // 앞으로 빼서 다시 뒤로 넣기
-      q1.push(tmp);
     }
+
     // k번째이면 결과 큐에 삽입
-    tmp = q1.front();
+    q2.push(q1.front());
     q1.pop();
-    q2.push(tmp);
-    cout << "큐 사이즈" << q1.size();
-    cout << "큐 사이즈" << q2.size();
-    if (q2.size() == n)
-    {
-      break;
-    }
   }
 
   // 결과 큐 출력
-  for (int i = 0; i < q2.size(); i++)
+  for (int i = 0; i <= q2.size(); i++)
   {
     cout << q2.front() << ", ";
     q2.pop();
+    cout << "큐사이즈" << q1.size() << q2.size();
   }
 
-  // 입력 큐에 남은 수 출력. ex) k=3 일 때 1, 4는 q1에 그대로 남아 있으나 q1의 남은 개수가 k보다 작아 출력되지 않음
-  for (int i = 0; i < q1.size() - 1; i++)
-  {
-    cout << q1.front();
-    q1.pop();
-  }
+  // // 입력 큐에 남은 수 출력. ex) k=3 일 때 1, 4는 q1에 그대로 남아 있으나 q1의 남은 개수가 k보다 작아 출력되지 않음
+  // for (int i = 0; i < q1.size() - 1; i++)
+  // {
+  //   cout << q1.front();
+  //   q1.pop();
+  // }
   cout << ">";
 
   return 0;
