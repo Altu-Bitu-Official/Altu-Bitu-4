51c4af571999dc747e73763f1f678c20f9c9e42c
doh-ha <hayeonlr@gmail.com>
2023-03-02 23:42:30 +0900

    [스택, 큐, 덱] 3월 2일 -Update

--- 02_스택, 큐, 덱/필수/1158.cpp
+++ 02_스택, 큐, 덱/필수/1158.cpp
@@ -26,7 +26,6 @@ int main()
   // 결과 큐에 입력
   while (!q1.empty())
   {
-    int tmp;
     for (int i = 0; i < k - 1; i++)
     {
       q1.push(q1.front());
@@ -37,22 +36,16 @@ int main()
     q2.push(q1.front());
     q1.pop();
   }
+  int last = q1.back();
 
   // 결과 큐 출력
-  for (int i = 0; i <= q2.size(); i++)
+  for (int i = 1; i < n; i++)
   {
     cout << q2.front() << ", ";
     q2.pop();
-    cout << "큐사이즈" << q1.size() << q2.size();
   }
 
-  // // 입력 큐에 남은 수 출력. ex) k=3 일 때 1, 4는 q1에 그대로 남아 있으나 q1의 남은 개수가 k보다 작아 출력되지 않음
-  // for (int i = 0; i < q1.size() - 1; i++)
-  // {
-  //   cout << q1.front();
-  //   q1.pop();
-  // }
-  cout << ">";
+  cout << last << ">";
 
   return 0;
 }
