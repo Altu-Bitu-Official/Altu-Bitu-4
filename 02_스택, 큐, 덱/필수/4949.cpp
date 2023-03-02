3ef159cb79f7b74a86fb1306e7f63c5a6d625695
doh-ha <hayeonlr@gmail.com>
2023-03-02 23:57:56 +0900

    [스택, 큐, 덱] 3월 2일 -Update
    4949

--- 02_스택, 큐, 덱/필수/4949.cpp
+++ 02_스택, 큐, 덱/필수/4949.cpp
@@ -4,48 +4,65 @@
 
 using namespace std;
 
-int main(){
-  //시간 초과 방지
+int main()
+{
+  // 시간 초과 방지
   ios::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
+  while (1)
+  { // 입력 개수가 주어지지 않음
 
+    // 입력
+    stack<char> st;
+    string sentence;
+    getline(cin, sentence); // 입력 가능한 최대 문자 수를 지정하지 않아도 됨.
+    // 개행 문자 만나기 전까지 모든 문자 읽어서 string 객체 (sentence) 에 저장
 
-while(1){ //입력 개수가 주어지지 않음
+    // 괄호가 하나도 없는 경우도 균형잡힌 문자열
+    if (sentence == ".")
+    {
+      cout << "yes\n";
+      break;
+    }
 
-//입력
-stack<char> st;
-string sentence;
-getline(cin,sentence); //입력 가능한 최대 문자 수를 지정하지 않아도 됨.
-//개행 문자 만나기 전까지 모든 문자 읽어서 string 객체 (sentence) 에 저장
+    // 글자 하나씩 탐색
+    for (int i = 0; i < sentence.length(); i++)
+    {
+      // 여는 괄호 나오면 스택에 푸쉬
+      if (sentence[i] == '(' || sentence[i] == '[')
+      {
+        st.push(sentence[i]);
+      }
+      // 닫는 괄호 나오면 스택에 여는 괄호가 있어야 팝
+      else if (sentence[i] == ')')
+      {
+        if (st.top() == '(')
+        {
+          st.pop();
+                }
+      }
 
+      else if (sentence[i] == ']')
+      {
+        if (st.top() == '[')
+        {
+          st.pop();
+        }
 
-//괄호가 하나도 없는 경우도 균형잡힌 문자열
-if(sentence=="."){
-  cout << "yes\n";
-  break;
-}
+        break;
+      }
+    }
 
-//글자 하나씩 탐색
-for(int i=0; i<sentence.length(); i++){
-  if(sentence[i]=='(' || sentence[i]=='['){
-    st.push(sentence[i]);
+    if (st.empty())
+    {
+      cout << "yes\n";
+    }
+    else
+    {
+      cout << "no\n";
+    }
   }
-
-  else if(sentence[i]==')' || sentence[i]==']'){
-    st.pop();
-  }
-
-}
-
-
-if(st.empty()){
-  cout << "yes\n";
-}
-else{ 
-  cout << "no\n";
-}
-
-}
-return 0;
+  return 0;
 }
