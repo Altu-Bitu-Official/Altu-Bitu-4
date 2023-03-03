54a559c0786283d90b83ba608440fe7644a02f25
trash1ife <djagksmf0817@ewhain.net>
2023-03-03 15:02:36 +0900

    [스택, 큐, 덱] 0228
    1158, 4949 피드백 반영

--- 02_스택, 큐, 덱/필수/1158.cpp
+++ 02_스택, 큐, 덱/필수/1158.cpp
@@ -4,59 +4,39 @@
 
 using namespace std;
 
-void removeNum(int n, int k, vector<int>& arr, queue<int>& q) {
-
-	int tip = 0; // 가리키는 위치
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < k; j++) {
-
-			if (tip >= n) { //가리키는 위치가 arr 길이를 넘어버리면 다시 첫번째 자리로 이동
-				tip = 0;
-			}
-
-			tip++; // 위치 이동
-
-			while (arr[tip] == -1) { //이미 지운 숫자는 넘어감
-				if (tip >= n) { //가리키는 위치가 arr 길이를 넘어버리면 다시 첫번째 자리로 이동
-					tip = 0;
-				}
-				tip++; // 위치 이동
-			}
+void yosephus(int n, int k, queue<int>& q) {
+	while (n != 1) { //큐에 원소가 하나 남을때까지 반복 
+		for (int i = 1; i < k; i++) { //k 전까지는 큐의 back으로 순서 미루기 
+			q.push(q.front());
+			q.pop();
 		}
-		q.push(arr[tip]); // 해당하는 위치의 숫자를 q에 넣음
-		arr[tip] = -1; //큐에 넣은 숫자는 지움
+		cout << q.front() << ", "; //k번째 원소 출력 후 제거 
+		q.pop();
+		n--;
 	}
 }
 
 int main()
 {
 	int n, k; // n개의 숫자와 제거할 k번째 숫자
-	vector<int> arr; // n번째까지의 원
-	queue<int> q; // 출력에 쓰일 큐
+	queue<int> q; // n번째까지의 원
 
 	//입력
 	cin >> n >> k;
 
 	// 1~n까지의 원 구성
-	arr.assign(n + 1, 0);
 
 	for (int i = 1; i <= n; i++) {
-		arr[i] = i;
+		q.push(i);
 	}
 
-	//연산
 
-	removeNum(n, k, arr, q);
-
-	//출력
+	//연산 및 출력
 	cout << "<";
-	for (int i = 1; i < n; i++) {
-		cout << q.front() << ", ";
-		q.pop();
-	}
+	
+	yosephus(n, k, q);
 
-	cout << q.front() << ">";
+	cout << q.front() << ">"; // 마지막 원소 출력 
 	q.pop();
 
 	return 0;
--- 02_스택, 큐, 덱/필수/4949.cpp
+++ 02_스택, 큐, 덱/필수/4949.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <stack>
+#include <map>
 
 using namespace std;
+
+bool cmpBalance(char str[500]) {
+	stack<char>s; // 괄호를 넣을 스택
+	map<char, char> m;
+	m[')'] = '(';
+	m[']'] = '[';
+
+	for (int i = 0; str[i] != '.'; i++) { //문장의 끝에 도달할 때 까지 반복
+		if (str[i] == '(' || str[i] == '[') {
+			s.push(str[i]);
+		}
+		else if (str[i] == ')' || str[i] == ']') {
+			if (!s.empty() && s.top() == m[str[i]]) { // 닫히는 괄호의 경우, 스택이 비어 있지 않고, top의 짝이 맞으면 pop
+				s.pop();
+			}
+			else {
+				return false; //아니면 균형이 맞지 않음 
+			}
+		}
+	}
+
+	return s.empty(); // 최종 연산 후 스택이 비어있다면 균형이 잡힌 것
+}
 
 
 int main()
 {
 	char str[500]; // 줄 입력 받을 문자 - 100자 이상이므로 101자보다 크게 설정 
-	stack<char>s; // 괄호를 넣을 스택
+
 
 	while (true) {
 		cin.getline(str, 501); // 한 줄 입력 받기
@@ -15,44 +39,14 @@ int main()
 			break;
 		}
 
-		for (int i = 0; str[i] != '.'; i++) { //문장의 끝에 도달할 때 까지 반복
-			switch (str[i])
-			{
-			case '(':
-			case '[':
-				s.push(str[i]); // 열린 괄호는 스택에 넣기
-				break;
-			case ')':
-				if (!s.empty() && s.top() == '(') { // 닫히는 괄호의 경우, 스택이 비어 있지 않고, top의 짝이 맞으면 pop
-					s.pop();
-				}
-				else {
-					s.push(str[i]); // 그렇지 않으면 그냥 stack에 push
-				}
-				break;
-			case ']':
-				if (!s.empty() && s.top() == '[') {
-					s.pop();
-				}
-				else {
-					s.push(str[i]);
-				}
-				break;
-			default:
-				break;
-			}
-		}
-
-		if (s.empty()) { // 최종 연산 후 스택이 비어있다면 균형이 잡힌 것
-			cout << "yes\n";
+		//연산 및 출력
+		if (cmpBalance(str)) {
+			cout << "yes";
 		}
 		else {
-			cout << "no\n";
-			}
-
-		while (!s.empty()) { //다음 줄의 연산을 위해 스택을 비워줌.
-			s.pop();
+			cout << "no";
 		}
+
 	}
 
 	return 0;
