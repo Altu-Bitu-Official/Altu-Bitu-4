797b90c81693b15d3a8c5c4a7b757ee98c378cc6
cakenpeace <chwon02@naver.com>
2023-03-03 13:58:29 +0900

    [스택, 큐, 덱]] 0303 - Update

--- 02_스택, 큐, 덱/필수/BOJ_4949.cpp
+++ 02_스택, 큐, 덱/필수/BOJ_4949.cpp
@@ -4,16 +4,9 @@
 
 using namespace std;
 
-int main() {
-    int len, i;
-    string str;
-    stack<char> stack;
-    while (1) {
-        getline(cin, str);
-        if (str[0] == '.') {
-            return 0;
-        } //.이면 프로그램 종료
-
+bool checkBalance (string str) {
+        int i, len;
+        stack<char> stack;
         len = str.length();
         for (i = 0; i < len; i++) {
             if (str[i] == '(' || str[i] == '[') {
@@ -23,28 +16,32 @@ int main() {
                 if (!stack.empty() && stack.top() == '(') {
                     stack.pop();
                 } else {
-                    cout << "no" << '\n';
-                    break; // ) 전이 (이 아니면 바로 반복 종료, 불균형 판정
+                    return false; // ) 전이 (이 아니면 바로 반복 종료, 불균형 판정
                 }
             }
             else if (str[i] == ']') {
                 if (!stack.empty() && stack.top() == '[') {
                     stack.pop();
                 } else {
-                    cout << "no" << '\n';
-                    break; // ) 전이 (이 아니면 바로 반복 종료, 불균형 판정
+                    return false; // ) 전이 (이 아니면 바로 반복 종료, 불균형 판정
                 }
             }
         }
-        if (i == len) { //중간에 멈추지 않고 반복이 끝까지 돌아갔을 때
-            if (stack.empty()) {
-                cout << "yes" << '\n';
-            } else {
-                cout << "no" << '\n';
-            }
-        }
-        while (!stack.empty()) {
-            stack.pop();
-        }
+        return stack.empty(); //중간에 멈추지 않고 반복이 끝까지 돌아갔을 때
+        
+}
+
+int main() {
+    int len, i;
+    string str;
+    while (1) {
+        // 문자열 읽어오기
+        getline(cin, str);
+        if (str[0] == '.') {
+            return 0;
+        } //.이면 프로그램 종료
+        
+        // 괄호열 체크하는 함수 호출 후 결과 출력
+        cout << (checkBalance(str) ? "yes" : "no") << '\n';
     }
 }
