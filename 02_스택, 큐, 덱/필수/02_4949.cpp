506175657a3d1ef8ba2f67af9cf8c64eafd8f963
JisooJeong <113654733+JisooJudy@users.noreply.github.com>
2023-03-03 16:15:51 +0900

    Update 02_4949.cpp

--- 02_스택, 큐, 덱/필수/02_4949.cpp
+++ 02_스택, 큐, 덱/필수/02_4949.cpp
@@ -4,51 +4,52 @@
 using namespace std;
 
 int main() {
-    stack<char> stack;
 
-    while(1) {
+    while (1) {
 
         string s;
         getline(cin, s);
-        if(s=="."){
+        bool tmp = true;
+
+        if (s == ".") {
             break;
         }
+        stack<char> stack;
 
+        for (int i = 0; i < s.length(); ++i) {
 
-        for (int i = 0; i < s.length(); i++) {
             if (s[i] == '(' || s[i] == '[') {
                 stack.push(s[i]);
             }
+
             else if (s[i] == ')') {
                 if (!stack.empty() && stack.top() == '(') {
                     stack.pop();
                 }
                 else {
-                    cout << "no\n";
+                    tmp = false;
                     break;
                 }
             }
+
             else if (s[i] == ']') {
                 if (!stack.empty() && stack.top() == '[') {
                     stack.pop();
                 }
                 else {
-                    cout << "no\n";
+                    tmp = false;
                     break;
                 }
             }
-            else {
-                cout << "no\n";
-                break;
-            }
-
         }
-        if (stack.empty()) {
+
+        if (stack.empty() && tmp==true) {
             cout << "yes\n";
         }
         else {
             cout << "no\n";
         }
     }
+
     return 0;
 }
