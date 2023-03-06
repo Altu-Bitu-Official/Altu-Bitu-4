03c1393e1e723687376bed194dab0f3408d68b49
cakenpeace <chwon02@naver.com>
2023-03-06 23:20:47 +0900

    [스텍, 큐, 덱] 0306 - Update

--- 02_스택, 큐, 덱/필수/BOJ_10757.cpp
+++ 02_스택, 큐, 덱/필수/BOJ_10757.cpp
@@ -2,28 +2,17 @@
 
 using namespace std;
 
-int main() {
-    int a[10001] = {0}, b[10001] = {0}, n[10001] = {0}, idx, t_len;
-    string t;
-    cin >> t;
-    t_len = t.length() - 1;
-    idx = 10001;
-    // 입력받은 숫자의 자리수만큼 반복, 배열에 숫자 복사
+// 입력받은 숫자의 자리수만큼 반복, 배열에 숫자 복사
+void copyNumber(const string& t, int* a) {
+    int t_len = t.length() - 1, idx = 10001;
     for (; t_len >= 0; t_len--) {
         a[idx] = t[t_len] - '0';
         idx--;
     }
-    idx = 10001;
+}
 
-    cin >> t;
-    t_len = t.length() - 1;
-    idx = 10001;
-    // 입력받은 숫자의 자리수만큼 반복, 배열에 숫자 복사
-    for (; t_len >= 0; t_len--) {
-        b[idx] = t[t_len] - '0';
-        idx--;
-    }
-
+// 두 숫자를 더하는 함수
+void addNumber(int* n, int* a, int* b) {
     for (int i = 10001; i > 0; i--) {
         n[i] += a[i] + b[i];
         // 한 자리의 숫자가 10 이상 -> 다음 자리 올림 후 자신에서 -10 
@@ -32,12 +21,26 @@ int main() {
             n[i] -= 10;
         }
     }
+}
+
+int main() {
+    int a[10001] = {0}, b[10001] = {0}, n[10001] = {0}, idx, t_len;
+    string t;
+
+    cin >> t;    
+    copyNumber(t, a);
+    cin >> t;
+    copyNumber(t, b);
+
+    addNumber(n, a, b);
 
     idx = 0;
 
     // 숫자가 시작되는 구간 찾기
     while (1) {
-        if (n[idx] != 0) break;
+        if (n[idx] != 0) {
+            break;
+        }
         idx++;
     } 
 
