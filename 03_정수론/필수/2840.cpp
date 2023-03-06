70d15df789891b3b021f2cc165e87dc7dbe1e884
Ryuryu128 <79790536+Ryuryu128@users.noreply.github.com>
2023-03-07 01:05:31 +0900

    [정수론] 3월 7일

--- 03_정수론/필수/2840.cpp
+++ 03_정수론/필수/2840.cpp
@@ -1,37 +1,78 @@
 #include "iostream"
 #include "vector"
 #include "queue"
+#include "set"
+#include "algorithm"
 
 using namespace std;
 
+// 2번째줄부터의 입력값을 담는 구조체
 struct Input {
-    int change;
-    char stop;
+    int change; // 문자가 바뀐 횟수
+    char stop;  // 회전을 멈췄을 때 가리킨 글자
 };
 
-vector<char> getWheelOfFortune(queue<Input> inputs, int n) {
-    vector<char> result(n, NULL);
-    Input input;
-    int sum;
+void getWheelOfFortune(queue<Input> inputs, int n) {
+    vector<char> result(n, NULL);   // 행운의 바퀴 결과를 담을 vector
+    Input input;                       // 현재 Input을 담을 Input
+    int sum = 0;                       // 문자가 바뀐 총 횟수를 int
+    set<char> check_char;              // 문자 중복 등록 체크를 위한 set
 
+    // inputs이 빌 때까지
     while (!inputs.empty()) {
+        // inputs의 맨 앞 Input값 가져오기
         input = inputs.front();
         inputs.pop();
-
+        // 문자가 바뀐 총 횟수 업데이트
         sum += input.change;
+
+        // sum%n => 원형칸을 빙글빙글 도는 것 구현(?)
+        // 아무것도 없다면 result에 현재 문자 삽입
         if (result[sum % n] == NULL) {
             result[sum % n] = input.stop;
-        } else if (result[sum % n] == input.stop) {
-            continue;
-        } else {
-            result.resize(1);
-            result[0] = '!';
-            return result;
+            // 중복 문자 체크 후에 check_char 업데이트
+            if(check_char.find(input.stop) == check_char.end())
+            {
+                check_char.insert(input.stop);
+                continue;
+            }
+            // continue에 걸리지 않았다면 => 중복된 문자가 다른 위치에 입력되었을 경우이므로 ! 반환
+            cout << '!';
+            return;
         }
-    }
-    return result;
-}
+        // 똑같은 문자가 이미 삽입되어 있다면 패스
+        else if (result[sum % n] == input.stop) {
+            continue;
+        }
+        // 삽입된 문자가 현재 문자와 다를 경우 ! 출력
+        else {
+            cout << '!';
+            return;
+        }
 
+        /** 질문1
+        바로 위에 두 else if와 else부분을
+        else if(result[sum % n] != input.stop){
+            cout << '!';
+            return;
+        }
+        로도 대체할 수 있을 것 같은데, 어느쪽이 더 좋은 방법일까요? 별로 상관없을까요?
+        **/
+
+        /** 질문2
+        while문 안의 첫번째 if문에 관한 질문입니다!
+        if문이 이중으로 되어있는데 혹시 이부분을 개선할 수 있는 방법이 있을까요? 이정도의 이중 if문은 괜찮을까요?
+        **/
+    }
+    // 현재는 반대 방향으로 문자들이 저장되어있으므로 reverse해준다
+    reverse(result.begin(), result.end());
+
+    int j;
+    for (int i = n - sum % n - 1; i < result.size() + (n - sum % n - 1); i++) {
+        j = i % n;
+        cout << (result[j] = (result[j] == NULL) ? '?' : result[j]);
+    }
+}
 
 int main() {
     //입출력 속도 향상
@@ -49,11 +90,6 @@ int main() {
         cin >> input.change >> input.stop;
         inputs.push(input);
     }
-
-    vector<char> result = getWheelOfFortune(inputs, n);
-
-    for (int i = 0; i < result.size(); i++) {
-        cout << (result[i] = (result[i] == NULL) ? '?' : result[i]);
-    }
-
+    // 연산, 출력
+    getWheelOfFortune(inputs, n);
 }
