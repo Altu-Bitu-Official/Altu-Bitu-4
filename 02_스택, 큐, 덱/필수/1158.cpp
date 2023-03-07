eadaf0982e0359e45df961dd6b336b411655a326
chaeyeon <321codus@naver.com>
2023-03-07 23:53:58 +0900

    [스택, 큐, 덱] 0307
    * 1158번 코드 2차 수정

--- 02_스택, 큐, 덱/필수/1158.cpp
+++ 02_스택, 큐, 덱/필수/1158.cpp
@@ -3,40 +3,50 @@
 #include <queue>
 using namespace std;
 
-void kill(queue<int>& q, const int k){
-	while(!q.empty()){
-        //kill 대상이 아닌 사람을 큐의 마지막에 다시 추가
-		for(int i=0; i<k-1; i++){
-			q.push(q.front());
-			q.pop();
-		}
-        //출력 포맷 맞추기용
-        if(q.size() == 1){
-            cout << q.front() << ">";
+vector<int> kill(const int n, const int k)
+{
+    queue<int> q;
+    // queue 채우기
+    for (int i = 1; i <= n; i++)
+    {
+        q.push(i);
+    }
+    vector<int> v;
+
+    while (!q.empty())
+    {
+        // kill 대상이 아닌 사람을 큐의 마지막에 다시 추가
+        for (int i = 0; i < k - 1; i++)
+        {
+            q.push(q.front());
+            q.pop();
         }
-        else{
-            cout << q.front() << ", ";
-        }
-        //kill
-		q.pop();
-	}
+        v.push_back(q.front());
+        // kill
+        q.pop();
+    }
+    
+    return v;
 }
 
 int main()
 {
     int n, k;
-    queue<int> q;
-
-    //입력
+    vector<int> v;
+    // 입력
     cin >> n >> k;
     
-    //출력 포맷 맞추기용
+    // 요세푸스 순열
+    v = kill(n, k);
+
+    //출력
     cout << "<";
-    
-    //queue 채우기
-    for(int i=1; i<=n; i++){
-        q.push(i);
+
+    for (int i=0; i<v.size()-1; i++)
+    {
+        cout << v[i] << ", ";
     }
-    //요세푸스 순열
-	kill(q, k);
+
+    cout << v[n-1];
+    cout << ">";
 }
