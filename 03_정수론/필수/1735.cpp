de885e7926204bdaedf9102ba0baadda6b203207
YujinJeon888888 <yujinjun@ewhain.net>
2023-03-07 18:24:28 +0900

    [정수론] 0307
    과제 통과 조건은 만족 못 하지만 질문이 있어 푸시합니다. ctrl+f 질문<-키워드로 검색하시면 주석으로 달아둔 질문 확인하실 수 있습니다. 둘 다 출력이 문젠데, 버퍼문제가 있는건지 뭔지 헷갈립니다 ㅠ..
    
    2609번과 2960번은 과제인 줄 알고 착각해서 푸시했던거 뺀 흔적입니다.

---
+++ 03_정수론/필수/1735.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+int gcdIter(int a, int b);
+
+int main() {
+	//분모: 최소공배수
+	//분자: (최소공배수/분모)*분자
+
+	int A_top;
+	int A_bottom;
+	int B_top;
+	int B_bottom;
+	int bottom_gcd;
+	int bottom_lcm;
+	int result_top;
+	int result_bottom;
+	//입력받기
+	cin >> A_top >> A_bottom;
+	cin >> B_top >> B_bottom;
+
+	//분모의 최소공배수 계산(최소공배수: 최대공약수*(A/최대공약수)*(B/최대공약수)
+	bottom_gcd = gcdIter(A_bottom, B_bottom);
+	bottom_lcm = bottom_gcd * (A_bottom / bottom_gcd) * (B_bottom / bottom_gcd);
+	//분자저장
+	result_top = (bottom_lcm / A_bottom) * A_top + (bottom_lcm / B_bottom) * B_top;
+	//분모저장
+	result_bottom = bottom_lcm;
+
+	//기약분수로 나타내자
+	//최대공약수로 계속 나눔. 최대공약수가 1이되는 순간 멈추기
+	while (1) {
+		int gcd;
+		gcd = gcdIter(result_bottom, result_top);
+		if (gcd == 1) {
+			break;
+		}		
+		result_top = result_top/gcd;
+		result_bottom = result_bottom / gcd;
+	}
+
+	//출력
+	//질문: 1 20 1 5 넣었을 때 디버그 하니 result_top안에 1이 들어있는데, 출력이 왜 안되는지 모르겠어요ㅠ_ㅠ
+	cout << result_top + " ";
+	cout << result_bottom;
+
+	return 0;
+}
+
+int gcdIter(int a, int b) {
+	while (b) {//b==0, a가 최대공약수
+		a %= b;
+		swap(a, b);
+	}
+	return a;
+}
