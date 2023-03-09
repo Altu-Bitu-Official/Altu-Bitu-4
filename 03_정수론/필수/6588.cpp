5d502e98df8ee947608630263c91e7e204dd3f03
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-03-09 21:20:47 +0900

    [정수론] 리뷰 반영 수정

--- 03_정수론/필수/6588.cpp
+++ 03_정수론/필수/6588.cpp
@@ -31,11 +31,12 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int a = 0, b;
+    int b;
     int n = 1;
     vector<int> is_prime = findPrime();
     while (n)
-    {
+    {   
+        int a=0;
         cin >> n;
         if (n == 0)
         {
