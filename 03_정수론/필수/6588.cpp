52c6cc0d46fd26d4b2115b89c4ea5435b23a9f9d
JJangdaye <moda1019@naver.com>
2023-03-10 21:17:37 +0900

    Update 6588.cpp

--- 03_정수론/필수/6588.cpp
+++ 03_정수론/필수/6588.cpp
@@ -3,16 +3,17 @@
 #include <vector>
 
 using namespace std;
-vector<int> prime; // 소수를 담을 수 있는 벡터
+
 bool check[1000001];
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    vector<int> prime; // 소수를 담을 수 있는 벡터
 
     for (long long i= 2; i <= 1000000; i++) {
         if (check[i])
             continue;
-        prime.push_back(i);
+        prime.push_back(i); //이 부분은 에라토스테네스의 체를 활용하는 부분이니까 따로 함수화 해주는게 더 좋음
         for (long long j= i * i; j <= 1000000; j += i) {
             check[j] = true;
         }
@@ -21,7 +22,9 @@ int main() {
     while (1) {
         int n;
         cin >> n;
-        if (n == 0) break;
+        if (n == 0) {
+            break; //break문 한 줄만 들어간다고 해도 중괄호 써주기
+        }
         int n2 = n / 2;
         for (int i = 0; i < n2; i++) {
             //n과 소수를 담고있는 벡터의 첫번째 요소부터 차이를 계산하면 나머지값 찾을 수 있음
