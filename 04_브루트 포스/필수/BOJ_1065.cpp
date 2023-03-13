5137b24b27dc8419156f0a180a09b964c9c06792
kimhj010502 <kimhj010502@ewhain.net>
2023-03-13 17:18:05 +0900

    [브루트 포스] 0313

---
+++ 04_브루트 포스/필수/BOJ_1065.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int hanSu(int n) {
+	int cnt = 0; //한수의 개수
+	for (int i = 1; i <= n; i++) {
+		string num = to_string(i);
+		int diff = num[1] - num[0]; //공차
+		bool check = true; //등차수열 여부
+		for (int j = 2; j < num.size(); j++) {
+			//공차가 달라지는 경우에는 등차수열 아님
+			if (num[j] - num[j - 1] != diff) { 
+				check = false;
+				break;
+			}
+		}
+		//등차수열인 경우에 한수의 개수 늘리기
+		if (check) {
+			cnt++;
+		}
+	}
+	return cnt;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int n;
+	cin >> n;
+
+	//연산 & 출력
+	cout << hanSu(n);
+
+	return 0;
+}
---
+++ 04_브루트 포스/필수/BOJ_1436.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int solution(int n) {
+	int cnt = 0; //영화의 개수
+	int i = 666; //탐색 시작 숫자
+	
+	while (cnt < n) {
+		//666이 들어간 숫자면 영화 개수 늘림
+		if (to_string(i).find("666") != string::npos) {
+			cnt++;
+		}
+		i++;
+	}
+	//while문을 빠져나오면서 i가 1 증가된채로 나오므로 1만큼 빼주기
+	return i - 1; 
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int n;
+	cin >> n;
+
+	//연산 & 출력
+	cout << solution(n);
+
+	return 0;
+}
