b5aab24d40b7090d7e9040e9f7b4d5d0cbb0bf96
zziyuni <xhrl6671@naver.com>
2023-03-14 16:27:16 +0900

    [브루트 포스] 3월 14일

--- 04_브루트 포스/필수/1063.cpp
+++ 04_브루트 포스/필수/1063.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 
 
-void movePosition(string &kPosition,string m){
-   if(m=="R"){
+void movePosition(string &kPosition,string m){ //위치를 옮기는 함수
+   if(m=="R"){ //if문을 통해 각 경우에 따라 동작하도록 함
         if(char(kPosition[0]+1) <='H') {
             string result="";
-            result+=kPosition[0]+1;
-            result+=kPosition[1];
+            result+=kPosition[0]+1; //질문: 두 char을 이어붙이는 더 쉬운 방법이 있을까요?
+            result+=kPosition[1]; //질문 : 여기서 result=(kPosition[0]+1)+kPosition[1] 형태로 작성하면 결과값이 왜 제대로 안나오는 지 궁금합니다.
             kPosition = result;
         }
     }else if(m=="L"){
@@ -84,13 +84,13 @@ int main(){
 
  for(int i=0;i<n;i++){
     cin >>move;
-    tmpK= kPosition;
-     movePosition(kPosition,move);
-     if(kPosition == sPosition){
-         tmpS =sPosition;
-         movePosition(sPosition,move);
-         if(sPosition==tmpS){
-             kPosition = tmpK;
+    tmpK= kPosition; //이동하기전 킹의 위치를 저장해두는 변수 tmpK
+     movePosition(kPosition,move); //킹 이동시킴
+     if(kPosition == sPosition){ //킹과 돌의 위치가 같다면
+         tmpS =sPosition; //이동하기전 돌의 위치를 저장해두는 변수 tmpS
+         movePosition(sPosition,move); //돌 이동시킴
+         if(sPosition==tmpS){ //돌을 이동시켰지만 이전이랑 위치가 같음( 돌이 범위를 벗어나서 이동하지않음)
+             kPosition = tmpK;//킹의 위치도 이전과 동일하게 돌려놓음
          }
      }
 
--- 04_브루트 포스/필수/1436.cpp
+++ 04_브루트 포스/필수/1436.cpp
@@ -7,13 +7,13 @@ int main(){
 
     int n; //입력받을 숫자
     cin >> n;
-    vector<int> tripleSix;
-    int i=666;
-while(true){
+    vector<int> tripleSix;//종말숫자 저장할 벡터
+    int i=666; //첫번째 종말 숫자 저장
+while(true){ //i를 하나씩 증가시키며 i에 666이 포함됐을 경우 tripleSix배열에 저장
     if(to_string(i).find("666")!= std::string::npos){
         tripleSix.push_back(i);
     }
-    if(tripleSix.size()==n){
+    if(tripleSix.size()==n){ //원하는 갯수만큼의 종말숫자가 저장되었다면 while문 break
         break;
     }
     i++;
