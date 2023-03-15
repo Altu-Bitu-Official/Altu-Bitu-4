da26c5e4ce63c13305e34cad28a61b4ec600228b
j2hyeon <jihyunkorea@naver.com>
2023-03-15 19:26:06 +0900

    [0315] 추가제출

--- 04_브루트 포스/필수/BOJ_1065.cpp
+++ 04_브루트 포스/필수/BOJ_1065.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-int find(int N)
-{
-    int i = 666;
-    int series = 0;
-    string target;
-    while(1)
+int check(int n)
+{                 // 셋째자리수 또는 1000인 경우 한수의 개수를 반환하는 함수
+    int sum = 99; // 이미 셋째 자리 수이므로 100이전까지의 숫자들은 모두 한수
+
+    for (int i = 100; i <= n; i++)
     {
-        target = to_string(i);
-        for(int j = 0; j < target.length()-2; j++)
-            if(target[j] == '6' && target[j+1] == '6' && target[j+2] == '6')
-            {
-                series++;
-                if(series == N)
-                    return i;
-                break;
-            }
-        i++;
+        int first = i / 100;        // 백의 자릿수
+        int second = (i / 10) % 10; // 십의 자릿수
+        int third = i % 10;         // 일의 자릿수
+
+        if ((first - second) == (second - third))
+        {             // 한수인 경우
+            sum += 1; // sum에 1추가
+        }
     }
+    return sum;
 }
-int main() {
-    int N;
-    cin >> N;
-    cout << find(N);
+
+int main()
+{
+    int n;
+    int sum = 0; // 한수의 개수
+    cin >> n;    // 사용자입력
+
+    if (n < 100)
+    { // n이 두자리 수 일 경우 모든 수가 한수
+        sum = n;
+    }
+    else
+    { // n이 세자리 수 일 경우
+        sum = check(n);
+    }
+
+    // 출력
+    cout << sum;
 }
