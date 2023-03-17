1dc2b85bce611c0deda9613e7df956692e766777
zoo0YOON <yjy011123@ewhain.net>
2023-03-17 12:41:08 +0900

    [브루트 포스] 0317 - Update

--- 04_브루트 포스/필수/1436.cpp
+++ 04_브루트 포스/필수/1436.cpp
@@ -1,27 +1,30 @@
+/* substr 사용해서 고쳐보기
+참고 : https://ponyozzang.tistory.com/676
+*/
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    int N, i, ans; cin >> N;
+    int N;
+    cin >> N;
 
-    while(N){
-        for ( i =666; ; i++){
-            while (i){
-                if (i%1000 == 666){
-                    ans = i;
-                    N--;
-                }
-                else{
-                    i=i/10;
-                }
+    for ( int i = 666; i < 2066666666; i++){
+        string s = to_string(i); // 숫자 타입의 데이터를 안전하게 스트링 타입으로 변경
+        int suc = 0; //연속하는 6의 개수
+        for ( int j = 0; j < s.length() - 2 ; j++){
+            if(s.substr(j,3) == "666"){
+                N--;
+                break;
             }
         }
+        if ( N == 0){
+            return cout << s, 0;
+        }
     }
-    cout << ans;   // 시간초과 해결 방법?
-    return 0;
 }
