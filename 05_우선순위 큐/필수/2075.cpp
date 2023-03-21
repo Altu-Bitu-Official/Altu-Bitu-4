48f4e752a4f529e5752f57599957fbcb8aaed1c7
uommou <ffffff.990917@gmail.com>
2023-03-21 13:29:14 +0900

    [우선순위큐] 1976435 황채원

---
+++ 05_우선순위 큐/필수/2075.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int usePq(int n) {
+	int ans;
+	priority_queue<int, vector<int>, greater<int>> pq; // 작은 수가 top에 오는 우선순위큐
+	for (int i = 0; i < n * n; i++) {
+		int num;
+		cin >> num;
+		pq.push(num);
+		// 크기를 n으로 유지하면 top에는 n번째로 큰 수가 존재한다.
+		if (pq.size() > n) {
+			pq.pop();
+		}
+	}
+
+	ans = pq.top();
+
+	return ans;
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+
+	cout << usePq(n);
+	return 0;
+}
---
+++ 05_우선순위 큐/필수/2607.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+using namespace std;
+
+vector<string> s;
+int similarWord(vector<string>& s) {
+	string strdef = s[0]; // 첫단어를 기준으로 
+	int words = 0;
+	for (int i = 1; i < s.size(); i++) {
+		int cnt = 0;
+		string strvar = s[i];
+		int l = strvar.length() - strdef.length();
+		// 길이 차이 2 이상이면 버리기
+		if (abs(l) > 1) {
+			continue;
+		}
+		// 길이차이 0이면
+		else if (l == 0) {
+			// find로 검사
+			for (int i = 0; i < strdef.length(); i++) {
+				if (strvar.find(strdef[i]) == string::npos) { // find 해서 찾는 것이 없으면 string::npos 를 반환한다.
+					cnt++;
+				}
+			}
+			if (cnt <= 1) { // 하나가 다른 건 괜찮다
+				words++;
+			}
+		}
+		// 길이차이 1이면
+		else if (l == 1) {
+			// find로 검사
+			for (int i = 0; i < strdef.length(); i++) {
+				if (strvar.find(strdef[i]) == string::npos) {
+					cnt++;
+				}
+			}
+			if (cnt == 0) { // 길이 차이가 1이면 하나를 빼야 같은 구성이 된다. 모든 문자가 있어야함. 
+				words++;
+			}
+		}
+		else if (l == -1) {
+			// find로 검사
+			for (int i = 0; i < strdef.length(); i++) {
+				if (strvar.find(strdef[i]) == string::npos) {
+					cnt++;
+				}
+			}
+			if (cnt <= 1) { // 길이 차이가 -1이면 하나를 더해야 같은 구성이 됨. 따라서 더할 문자 하나 빼고는 모두 있어야한다.
+				words++;
+			}
+		}
+	}
+	return words;
+}
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		s.push_back(str);
+	}
+	cout << similarWord(s);
+	return 0;
+}
