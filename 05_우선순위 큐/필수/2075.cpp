1e1e8f06e3c27404b347e9f6a89975a06e83a2d4
YujinJeon888888 <yujinjun@ewhain.net>
2023-03-21 13:59:04 +0900

    [우선순위큐] 0321
    오름차순 ver

--- 05_우선순위 큐/필수/2075.cpp
+++ 05_우선순위 큐/필수/2075.cpp
@@ -1,53 +1,30 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 int main() {
-	priority_queue<int> pq; //큰 숫자에 우선순위.
-	int n;//n번째
+	//오름차순으로 하면 temp벡터 채우고 비우고 연산을 안 해도 되게 된다-> 시간복잡도 문제 해결
+	priority_queue<int, vector<int>, greater<int>> pq;//오름차순
+	//시간초과
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
 	cin >> n;
-	vector<int> temp(n);//팝 담아두는 공간
-	//숫자넣기
-	//상위 n번째 수만 출력함-> 상위 N번째 미만이면 없어도됨
-	int n_th_num = -1000000001;
-	for (int i = 1; i <= n; i++) {
-		for (int k = 0; k < n; k++) {
-			int input;
-			cin >> input;
-			if (pq.size() >= n && input > n_th_num) {
-				for (int j = 1; j <= n; j++) {//n번째만 골라내서 그거보다 작은 인풋이면 넣지말기
-					if (n == j) {
-						n_th_num = pq.top();//n번째 수 겟
-					}
-					temp.push_back(pq.top());
-					pq.pop();
-				}
-				//pq 복구
-				if (!temp.empty()) {
-					for (int j = temp.size() - 1; j >= 0; j--) {
-						pq.push(temp[j]);
-						temp.pop_back();
-					}
-				}
-			}
-			//벡터 비우기
-			temp.clear();
-
-			if (n_th_num < input) {//들어온 수가 n번째 수보다 작으면 넣을 필요가 없다. 
-				//따라서 n번째 수보다 클 때에만 넣는다.
+	for (int i = 0; i < n * n; i++) {
+		int input;
+		cin >> input;
+		if (i < n) {
+			pq.push(input);
+		}
+		else {
+			//이 때부터는 상위 n개 안에 못 들어가면 안 넣어도 됨
+			if (pq.top() < input) {
 				pq.push(input);
+				pq.pop();//하나 채웠으니까 n개로 다시 맞춰준다
+				//즉, 꼭대기는 n번째 수임-> 하나 들어오면 n+1번째 수 됨-> 팝하기
 			}
 		}
 	}
-
-	//답안출력
-	for (int i = 1; i <= n; i++) {
-		if (i == n) {
-			cout << pq.top();//n번째 수 출력 후 탈출
-			break;
-		}
-		pq.pop();
-	}
-
+	//탑 출력: 상위 n번째
+	cout << pq.top();
 	return 0;
 }
