91e2659f357fd5b62cd6bbfa4a88011ce2355e6c
doyeoo <ehdusla013@gmail.com>
2023-03-21 17:04:05 +0900

    [브루트포스] 필수 1063 수정

--- 04_브루트 포스/필수/1063.cpp
+++ 04_브루트 포스/필수/1063.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <utility>
 
 using namespace std;
 
+bool checkValid(char x, char y, int dx, int dy) {
+    return (x+dx)>64 && (x+dx)<73 && (y+dy)>48 && (y+dy)<57;
+}
+
 int main() {
-    string king, stone;
+    string tmp1, tmp2;
     string ch;
     int x, y, n;
 
-    cin >> king >> stone >> n;
+    cin >> tmp1 >> tmp2 >> n;
 
-    char king_x=king[0], king_y=king[1];
-    char stone_x=stone[0], stone_y=stone[1];
+    pair<char, char> king;
+    pair<char, char> stone;
+
+    king = make_pair(tmp1[0], tmp1[1]);
+    stone = make_pair(tmp2[0], tmp2[1]);
 
     for(int i=0; i<n; i++) {
         cin >> ch;
@@ -43,27 +51,27 @@ int main() {
         }
 
         //옮길 킹 위치가 범위 안이고
-        if((king_x+x)>64 && (king_x+x)<73 && (king_y+y)>48 && (king_y+y)<57) {
+        if(checkValid(king.first, king.second, x, y)) {
             //킹 옮긴 위치가 돌 위치랑 겹치면
-            if(stone_x==(king_x+x) && stone_y==(king_y+y)){
+            if(stone.first==(king.first+x) && stone.second==(king.second+y)){
                 //돌 똑같이 옮겼을 때 범위 안이면
-                if((stone_x+x)>64 && (stone_x+x)<73 && (stone_y+y)>48 && (stone_y+y)<57) {
+                if(checkValid(stone.first, stone.second, x, y)) {
                     //돌도 이동
-                    stone_x+=x;
-                    stone_y+=y;
+                    stone.first+=x;
+                    stone.second+=y;
                 } else {
                     //범위 안에 없으면 둘 다 안 옮김
                     continue;
                 }
             }
             //돌 위치랑 안 겹치면 킹만 이동
-            king_x+=x;
-            king_y+=y;
+            king.first+=x;
+            king.second+=y;
         }
     }
 
-    cout << king_x << king_y << "\n";
-    cout << stone_x << stone_y;
+    cout << king.first << king.second << "\n";
+    cout << stone.first << stone.second;
 
     return 0;
 }
