e076b85a3e040dc2eb385eb083e6a675a103ad5e
MinJung <52921222+Minjungh63@users.noreply.github.com>
2023-03-28 19:17:02 +0900

    주석 수정

--- 06_그리디 알고리즘/필수/17451.py
+++ 06_그리디 알고리즘/필수/17451.py
@@ -9,7 +9,7 @@ for i in range(1, n):
         continue
     elif v_min < v[i]: # 속도를 떨어뜨릴 수 있으므로
         v_min = v[i]
-    else: # v[i] * (m-1) <= v_min < v[i] * m 인 m 찾기
+    else: # v[i] * (m-1) < v_min <= v[i] * m 인 m 찾기
         m = math.ceil(v_min / v[i])
         v_min = v[i] * m
 print(v_min)
