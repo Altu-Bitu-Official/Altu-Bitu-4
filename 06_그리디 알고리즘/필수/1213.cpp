45bb4e966fdc297c03e4120bba747d23c586972b
Ryuryu128 <79790536+Ryuryu128@users.noreply.github.com>
2023-03-28 11:51:35 +0900

    [그리디 알고리즘] 3월 28일

--- 06_그리디 알고리즘/필수/1213.cpp
+++ 06_그리디 알고리즘/필수/1213.cpp
@@ -5,20 +5,24 @@
 
 using namespace std;
 
-bool judgePalindrome(string name, map<char, int> &check, vector<char> &keys) {
+// 팰린드롬을 만들 수 있는지 판단하는 함수
+bool judgePalindrome(string name, map<char, int> &check) {
     int count = name.size() % 2;    // 홀수개의 글자를 용납할 수 있는 횟수 : 글자 길이가 짝수면 0, 홀수면 1
+                                    // 홀수 단어라면 중앙자리가 생겨 홀수개의 글자 1개가 있을 수 있지만, 짝수는 모든 글자 개수가 2의 배수가 되어야하므로 X
 
+    // name의 i번째 글자가 check에 key로 존재하지 않는다면 새로운 key로 등록, value는 항상 1
+    // name의 i번째 글자가 check에 key로 존재한다면 해당하는 value를 ++
     for (int i = 0; i < name.size(); i++) {
         if (check.find(name[i]) == check.end()) {
             check[name[i]] = 1;
-            keys.push_back(name[i]);
-        }
-        else {
+        } else {
             check[name[i]]++;
         }
     }
-    for (int i = 0; i < keys.size(); i++) {
-        if ((check[keys[i]] % 2) == 1) {
+    // check를 돌면서 홀수개의 글자가 있다면 count--
+    // count가 0보다 작을 경우 허용하는 홀수 개의 단어 수보다 많다는 뜻이므로 팰린드롬을 만들 수 없다.
+    for (auto iter: check) {
+        if ((iter.second % 2) == 1) {
             count--;
             if (count < 0) {
                 return false;
@@ -28,43 +32,44 @@ bool judgePalindrome(string name, map<char, int> &check, vector<char> &keys) {
     return true;
 }
 
-void setWord(char c, int &count, string &palindrome, int k){
-    for(int i=0; i<k; i++){
+// 글자들을 palindrome에 넣어주는 함수
+void setWord(char c, int &count, string &palindrome, int k) {
+    for (int i = 0; i < k; i++) {
         palindrome[count] = c;
         palindrome[palindrome.size() - count] = c;
         count++;
     }
 }
 
-string makePalindrome(string &name, map<char, int> &check, vector<char> &keys) {
-    string palindrome{"", name.size()+1};
-    int count = 1;
-    sort(keys.begin(), keys.end());
+// 팰린드롬 단어를 만드는 함수
+string makePalindrome(string &name, map<char, int> &check) {
+    string palindrome("", name.size() + 1); // setWord에서 앞뒤 자리 계산이 용이하도록 name크기 +1로 설정
+    int count = 1;  // 글자들을 넣을 위치
 
-    for(int i=0; i<keys.size(); i++){
-        if(check[keys[i]] % 2 == 1){    // 홀수 개수
-            setWord(keys[i], count, palindrome, check[keys[i]]/2);
-            palindrome[(name.size()/2)+1] = keys[i];
-        }
-        else{   // 짝수 개수
-            setWord(keys[i], count, palindrome, check[keys[i]]/2);
+    // check를 돌면서 key에 해당하는 글자가
+    // 홀수개라면 setWord를 해주고 중앙에 남은 하나를 넣어준다
+    // 짝수개라면 setWord를 해준다.
+    for (auto iter: check) {
+        if (iter.second % 2 == 1) {
+            setWord(iter.first, count, palindrome, iter.second / 2);
+            palindrome[(name.size() / 2) + 1] = iter.first;
+        } else {
+            setWord(iter.first, count, palindrome, iter.second / 2);
         }
     }
+    // palindrome[0]을 지우고 return
     palindrome.erase(palindrome.begin());
-
     return palindrome;
 }
 
 int main() {
     string name;
+    map<char, int> name_map;
     // 입력
     cin >> name;
-    map<char, int> name_map;
-    vector<char> name_key;
-
     // 연산과 출력
-    if (judgePalindrome(name, name_map, name_key)) {
-        cout << makePalindrome(name, name_map, name_key);
+    if (judgePalindrome(name, name_map)) {
+        cout << makePalindrome(name, name_map);
     }
     else {
         cout << "I'm Sorry Hansoo";
--- 06_그리디 알고리즘/필수/17451.cpp
+++ 06_그리디 알고리즘/필수/17451.cpp
@@ -5,9 +5,12 @@
 using namespace std;
 
 long long getStartV(stack<int>&velocity){
-    long long start_v = velocity.top();   // start_v 초기화
-    velocity.pop();
+    long long start_v = velocity.top();   // start_v를 Vn으로 초기화
+    velocity.pop(); // Vn을 pop
 
+    // velocity가 빌 때까지
+    // velocity.top()이 현재 start_v보다 크거나 같다면 top을 start_v에 저장. => 큰 속도에서 작은 속도로 갈때는 배수 고려없이 그냥 낮추면 됨.
+    // velocity.top()이 현재 start_v보다 작다면 start_v는 현재 start_v보다 큰 top의 배수 중 가장 작은 값이다.
     while(!velocity.empty()){
         if(velocity.top() >= start_v){
             start_v = velocity.top();
@@ -33,11 +36,10 @@ int main(){
 
     // 입력
     cin >> n;
-
     while(n--){
         cin >> tmp;
         velocity.push(tmp);
     }
-
+    // 연산과 출력
     cout << getStartV(velocity);
 }
