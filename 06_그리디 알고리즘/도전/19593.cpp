04bb2f76c7d4afcf5fb6cce12278fc16ddf6043e
Ryuryu128 <79790536+Ryuryu128@users.noreply.github.com>
2023-03-28 18:01:02 +0900

    [그리디 알고리즘] 3월 28일

--- 06_그리디 알고리즘/도전/19593.cpp
+++ 06_그리디 알고리즘/도전/19593.cpp
@@ -1,26 +1,7 @@
 #include "iostream"
-#include "vector"
 #include "algorithm"
 
 using namespace std;
-
-bool isPossible(vector<int> &trees, int sum){
-    if(sum%3){
-        return false;
-    }
-    std::sort(trees.begin(), trees.end());
-    pair<int,int> count(0,0); // first:1 second:2
-    int tmp= 0;
-
-    for(int i=0; i<trees.size(); i++){
-        trees[i] -= (3-tmp);
-        tmp = trees[i]%3;
-    }
-    if(tmp){
-        return false;
-    }
-    return true;
-}
 
 int main(){
     //입출력 속도 향상
@@ -29,20 +10,21 @@ int main(){
     cout.tie(NULL);
 
     //입력
-    int n, tmp, sum =0;
-    vector<int> trees;
+    int n, tmp;
+    int sum = 0, count = 0;
     cin >> n;
+
     while(n--){
         cin >> tmp;
+        count += tmp/2; // 입력값을 2로 나눈 몫을 count에 더해준다
         sum += tmp;
-        trees.push_back(tmp);
     }
 
-    // 연산과 출력
-    if(isPossible(trees,sum)){
-        cout << "YES";
-    }
-    else{
+    if(sum%3){  // sum은 2+1 즉 3의 배수여야함
         cout << "NO";
+        return 0;
     }
+    // 2짜리 물뿌리개를 쓸 수 있는 경우가 총 물을 뿌리는 횟수인 sum/3보다 크거나 작게 되면 조건을 만족할 수 있다.
+    string answer = ((sum/3)<=count) ? "YES" : "NO";
+    cout << answer;
 }
