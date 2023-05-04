2218a62eb088f6dd0f54826ef4d376cb1c38bec6
sforseohn <sforseohn@gmail.com>
2023-05-04 11:37:17 +0900

    [동적 계획법] 추가 제출

---
+++ 09_동적 계획법/필수/11726.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int countNumberOfCases(int n) {
+    const int MOD = 10'007; // 오버플로우 방지
+    vector<int> dp(n+1); // 2xi 크기의 직사각형을 채우는 방법의 수
+    
+    dp[1] = 1; // 세로로 한 개만 놓는 경우
+    dp[2] = 2; // 세로로 두 개 or 가로로 두 개 놓는 경우
+    
+    for (int i = 3; i <= n; i++) { // 3부터 n까지 반복
+        int vertical = dp[i - 1]; // 세로로 한 개 놓는 경우
+        int horizontal = dp[i - 2]; // 가로로 두 개 놓는 경우
+        dp[i] = (vertical + horizontal) % MOD;  // 오버플로우 방지를 위해 MOD 연산
+    }
+    
+    return dp[n];
+}
+
+/**
+ * 가장 마지막 세로줄을 채울 수 있는 방법을 살펴봅시다.
+ * 1. 세로로 한 개를 놓는다.
+ * 2. 가로로 두 개를 놓는다.
+ *
+ * 1번 방법으로 마지막 세로줄을 채울 경우, 남은 부분의 크기는 2 x (n-1)
+ * 2번 방법으로 마지막 세로줄을 채울 경우, 남은 부분의 크기는 2 x (n-2)
+ *
+ * 따라서 2 x n 크기의 직사각형을 채우는 방법의 수는
+ * d[i] = dp[i - 1] + dp[i - 2]
+ */
+int main() {
+    int n; // 2×n 크기의 직사각형
+
+    // 입력
+    cin >> n;
+
+    // 연산
+    int answer = countNumberOfCases(n);
+
+    // 출력
+    cout << answer;
+
+    return 0;
+}
