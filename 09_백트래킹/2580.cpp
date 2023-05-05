36b5938802ed45dabbf1d3d953012de397b4f927
베뉴 <113707388+chock-cho@users.noreply.github.com>
2023-05-05 10:35:51 +0900

    Update 2580.cpp

--- 09_백트래킹/2580.cpp
+++ 09_백트래킹/2580.cpp
@@ -1,45 +1,74 @@
 // 2580 스도쿠
-// 왜!!!!!! 안되는지 모르겠습니다...ㅠㅠ 코멘트 부탁드리겠습니다...!ㅠㅠ 
+// 수정 사항: 틀렸지만, 제출일자까지 수정한 코드를 올립니다.
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 typedef pair<int,int> pi;
-bool isPossible(vector<vector<int>> &sudokuBoard, int x, int y){
-    for(int i = 0; i < 9; i++){
-        if(sudokuBoard[x][y] == sudokuBoard[x][i] && i != y){ //같은 열 체크
-            return false; 
+const int SIZE = 9;
+
+void printSudoku(vector<vector<int>> &sudokuBoard){
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            cout << sudokuBoard[i][j] << " ";
         }
-        if(sudokuBoard[x][y] == sudokuBoard[i][y] && i != x){ // 같은 행 체크
-            return false;
-        }
+        cout << "\n";
     }
-    // 구역 체크
-    int x_zone= x/3;
-    int y_zone = y/3;
-    for(int i = (3*x_zone); i <= (3*x_zone)+3; i++){
-        for(int j = (3*y_zone); j <= (3*y_zone+3); j++){
-            if(sudokuBoard[x][y] == sudokuBoard[i][j]) {
-                if(x != i && y != j){
-                    return false;
-                }
-            }
-        }
+}
+
+bool chkRow(vector<vector<int>> &sudokuBoard, int x, int y){
+  for(int i = 0; i < SIZE; i++){
+    if(sudokuBoard[x][y] == sudokuBoard[x][i]) {
+      if(i != y){
+        return false;
+      }
+      return true;
     }
     return true;
+  }
 }
+
+bool chkCol(vector<vector<int>> &sudokuBoard, int x, int y){
+  for(int i = 0; i < SIZE; i++){
+    if(sudokuBoard[x][y] == sudokuBoard[i][y]) {
+      if(i != x){
+        return false;
+      }
+      return true;
+    }
+    return true;
+  }
+}
+
+bool chkArea(vector<vector<int>> &sudokuBoard, int x, int y){
+  // (x_zone, y_zone) : (x, y)가 속한 정사각형의 왼쪽 상단의 꼭짓점 좌표
+  int x_zone = x / 3 * 3;
+  int y_zone = y / 3 * 3;
+
+  for(int i = 0; i < (SIZE)/3; i++){
+    for(int j = 0; j < (SIZE)/3; j++){
+      if(sudokuBoard[x_zone + i][y_zone + j]== sudokuBoard[x][y]){
+        if(x_zone+i != x || y_zone+j != y){
+          return false;
+        }
+        return true;
+      }
+    }
+  }
+  return true;
+}
+
+bool isPossible(vector<vector<int>> &sudokuBoard, int x, int y){
+  return chkCol(sudokuBoard, x, y) && chkRow(sudokuBoard, x, y) && chkArea(sudokuBoard, x, y);
+}
+
 void dfs(vector<vector<int>> &sudokuBoard, vector<pi> &vec, int &N, int depth){
     bool flag =false;
     if(depth == N){
-      flag =true;
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                cout << sudokuBoard[i][j] << " ";
-            }
-            cout << "\n";
-        }
+        flag =true;
+        return;
     }
-    for(int i = 1; i <= 9; i++){
+    for(int i = 1; i <= SIZE; i++){ //1부터 9까지의 수를 다 넣어보고, 그 후보군이 답을 만족시키는지/아닌지 판단
         sudokuBoard[vec[depth].first][vec[depth].second] = i;
         if(isPossible(sudokuBoard, vec[depth].first, vec[depth].second)) {
             dfs(sudokuBoard, vec, N, depth+1);
@@ -49,21 +78,22 @@ void dfs(vector<vector<int>> &sudokuBoard, vector<pi> &vec, int &N, int depth){
           sudokuBoard[vec[depth].first][vec[depth].second] = 0;
         }
     }
-
 }
+
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(0);
-    vector<vector<int>> sudokuBoard(9, vector<int>(9, 0));
+    vector<vector<int>> sudokuBoard(SIZE, vector<int>(SIZE, 0));
     int N = 0; //몇개 채워야 하는지를 담은 변수
     vector<pi> vec; // 채워야 할 board의 좌표값을 담은 vector 배열
-    for(int i = 0; i < 9; i++){
-        for(int j = 0; j < 9; j++){
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
           cin >> sudokuBoard[i][j];
-            if(sudokuBoard[i][j] == 0){
+            if(sudokuBoard[i][j] == 0){ // 채워야 할 부분이면 해당 좌표 쌍들을 vec에 저장
                 N++;
                 vec.push_back({i, j}); 
             }
         }
     }
     dfs(sudokuBoard, vec, N, 0);
+    printSudoku(sudokuBoard);
 }
