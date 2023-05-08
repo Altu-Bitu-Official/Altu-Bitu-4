59f0512343c63e67630c3d4233e3e36a704fbca7
베뉴 <113707388+chock-cho@users.noreply.github.com>
2023-05-08 23:36:24 +0900

    [0508] 이분 탐색 - 필수

--- 10_이분 탐색/필수/14500.cpp
+++ 10_이분 탐색/필수/14500.cpp
@@ -1,96 +1,70 @@
-// 14500 테트로미노 (구현, 백트래킹)
+// 14500 테트로미노
 /*
-    'ㅜ' 모양을 제외한 나머지는 depth가 0부터 시작하여 4으로 끝나는 백트래킹으로 처리할 수 있습니다.
-    'ㅜ' 모양을 처리 할 때에는, '+' 모양을 먼저 만들어준 다음에 
-    가운데 부분에서 가장 최소값을 빼어주는 형태로 구현해줍니다.
+  'ㅗ' 모양을 제외한 나머지 모양은 depth가 0부터 시작하여 3로 끝나는 dfs로 만들 수 있습니다.
 */
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
 using namespace std;
-
-const int dx[] = {1, -1, 0, 0}; // 동 서 남 북
-const int dy[] = {0, 0, -1, 1}; 
-int answer = 0;
-
-// 'ㅜ'를 제외한 나머지 모양은 depth가 4인 dfs로 만들 수 있습니다.
-void makeStandardShape(int &x, int &y, int depth, int sum, int &N, int &M, int TetroMino[][501], bool visited[][501]){
-    if(depth == 4){
-        answer = max(answer, sum);
-        return;
+const int MAX = 504;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
+int makeStandardShape(int x, int y, int prev_x, int prev_y, int depth, int TetroMino[MAX][MAX], int &N, int &M){
+  int answer = 0;
+  if(depth == 3){
+    return TetroMino[x][y];
+  }
+  for(int i = 0; i < 4; i++){
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+      if(nx < 0 || ny < 0 || nx >= N || ny >= M) {
+        continue;
+      }
+      if(prev_x == nx && prev_y == ny) {
+        continue;
+      }
+      answer = max(answer, TetroMino[x][y] + makeStandardShape(nx, ny, x, y, depth+1, TetroMino, N, M));
     }
-    
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-
-        if(nx < 0 || ny < 0 || nx >= N || ny >= M){
-            // 맵의 탐색 범위를 벗어나면 제끼고
-            continue; 
-        }
-        if(visited[nx][ny]){
-            // 이미 방문한 좌표이면 제끼고
-            continue;
-        }
-
-        visited[nx][ny] = true; // 방문 처리를 해준 후에,
-        makeStandardShape(nx, ny, depth + 1, sum + TetroMino[nx][ny], N, M, TetroMino, visited);
-        visited[nx][ny] = false;
-    }
+  return answer;
 }
 
-// 'ㅜ' 모양은 '+'모양에서, 동서남북 방향에서 하나씩 빼어주면 됩니다.
-void makeWOOShape(int &x, int &y, int sum, int &N, int &M, int TetroMino[][501], bool visited[][501]){
-    sum = TetroMino[x][y]; // 해당 좌표는 'ㅜ' 모양에서 가운데 부분의 좌표입니다.
-    int sub = 987654321; // 가운데 부분에서 상, 하, 좌, 우로 뻗어나갈 좌표 중 가장 최솟값을 골라냅니다.
-    // 가장 최솟값을 제거하면, 만들 수 있는 'ㅜ' 'ㅓ' 'ㅗ' 'ㅏ' 모양 중 가장 큰 값을 골라 낼 수 있습니다.
-
-    /* '+' 모양 만들어주기 */ 
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx < 1|| ny < 1 || nx > N || ny > M){
-            // 한 칸 이동했을 때의 값이 맵의 탐색 범위를 넘어가면 제끼고
-            continue;
-        }
-        if(visited[nx][ny]) {
-            continue;
-        } 
-        sub = min(sub, TetroMino[nx][ny]); // 가장 작은 값을 골라냅니다.
-        sum += TetroMino[nx][ny]; // 상하좌우에 있는 값을 모조리 더하면 '+' 모양이 완성됩니다.
+int makeWOOShape(int x, int y, int N, int M, int answer, int Tetromino[MAX][MAX]){
+    if (x - 1 >= 0 && y - 1 >= 0 && y + 1 < M) {
+         int tmp = Tetromino[x - 1][y] + Tetromino[x][y - 1] + Tetromino[x][y] + Tetromino[x][y + 1];
+          answer = max(answer, tmp);
     }
-    /* 이제 상하좌우에 있는 값 중 작은 값을 빼어서 'ㅓ' 'ㅗ' 'ㅏ' 'ㅜ' 모양을 만듭니다. */
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-         if(nx < 1 || ny < 1 || nx > N || ny > M){
-            continue;
-            answer = max(answer, sum - sub);
-        }
+    if (y - 1 >= 0 && x + 1 < N && y + 1 < M) {
+         int tmp = Tetromino[x][y - 1] + Tetromino[x][y] + Tetromino[x][y + 1] + Tetromino[x + 1][y];
+         answer = max(answer, tmp);
     }
+    if (x - 1 >= 0 && y - 1 >= 0 && x + 1 < N) {
+         int tmp = Tetromino[x - 1][y] + Tetromino[x][y - 1] + Tetromino[x][y] + Tetromino[x + 1][y];
+          answer = max(answer, tmp);
+    }
+    if (x - 1 >= 0 && x + 1 < N && y + 1 < M) {
+          int tmp = Tetromino[x - 1][y] + Tetromino[x][y] + Tetromino[x][y + 1] + Tetromino[x + 1][y];
+          answer = max(answer, tmp);
+    }
+  return answer;
 }
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(0);
-    // 입력 받는다.
-    int N, M;
-    cin >> N >> M;
-    int TetroMino[501][501];
-    bool visited[501][501] = {false, }; 
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= M; j++){
-            cin >> TetroMino[i][j];
-        }
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int N, M;
+  int Tetromino[MAX][MAX];
+  int answer = 0;
+  cin >> N >> M;
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      cin >> Tetromino[i][j];
     }
-
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= M; j++){
-            makeWOOShape(i, j, 0, N, M, TetroMino, visited);
-            visited[i][j] = true;
-            makeStandardShape(i, j, 0, 0, N, M, TetroMino, visited);
-            visited[i][j] = false;
-        }
+  }
+  for(int i = 0; i < N; i++) {
+  	for(int j = 0; j <= M; j++) {
+      answer = max(answer, makeStandardShape(i, j, -1, -1, 0, Tetromino, N, M));
+      answer = max(answer, makeWOOShape(i, j, N, M, 0, Tetromino));
     }
-
-    cout << answer << "\n";
+  }
+  cout << answer << "\n";
 }
