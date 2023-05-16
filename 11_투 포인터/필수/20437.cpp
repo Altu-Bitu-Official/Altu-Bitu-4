9d412409ead6ab2d6b993216fd0621744bd088c6
YujinJeon888888 <yujinjun@ewhain.net>
2023-05-16 18:10:29 +0900

    [투포인터] 0516--푸는중
    시간초과

--- 11_투 포인터/필수/20437.cpp
+++ 11_투 포인터/필수/20437.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 pair<int, int> countString(string& w, int k) {
 	//둘 다 끝 문자가 같아야함
+	//r: 오른쪽 끝, l: 왼쪽 끝
+	//r은 바깥 for, 하나씩줄임
+	// l은 이너뤂, 징 스캔
+	// r이 첫번째문자 닿을때까지
 	//r이동...
 	//어떤 문자 k개
 	//->r과 같은 문자 가리킬 때까지
@@ -15,24 +19,30 @@ pair<int, int> countString(string& w, int k) {
 	//어떤 문자 k개
 	//->r과 같은 문자 가리킬 때까지
 	//l 이동
-	int r=0, l=0;
-	int max_v=0, min_v= 10000;
+	int r = w.length() - 1, l = 0;
+	int max_v = 0, min_v = 10000;
 	vector<int> alpha(26, 0);
-	while (r!=(w.length())) {
-		alpha[w[r] - 'a']++;
-		if (alpha[w[r] - 'a'] < k) {
-			r++;
-			continue;
+
+	while (r != 0) {
+		for (l = 0; l != r; l++) {
+			alpha.assign(26, 0);//alpha초기화
+			//양 끝 문자 같아야하고
+			if (!(w[l] == w[r])) {
+				continue;
+			}
+			//k개 이어야 함
+			for (int i = l; i <= r; i++) {
+				alpha[w[i] - 'a']++;
+			}
+			if (alpha[w[l] - 'a'] == k) {
+				//만족하면 기록
+				max_v = max(max_v, r - l + 1);
+				min_v = min(min_v, r - l + 1);
+			}			
 		}
-		while (w[r] != w[l]||alpha[w[r]-'a']>k) {
-			alpha[w[l] - 'a']--;
-			l++;
-		}
-		max_v = max(max_v, r - l + 1);
-		min_v = min(min_v, r - l + 1);
-		r++;
+		r--;
 	}
-	pair<int, int> ans(0,0);
+	pair<int, int> ans(0, 0);
 	ans.first = min_v;
 	ans.second = max_v;
 	return ans;
@@ -42,7 +52,7 @@ int main() {
 	int n, k;//게임횟수, k개 문자
 	string w;//문자열
 	cin >> n;
-	string ans="";
+	string ans = "";
 	for (int i = 0; i < n; i++) {
 		cin >> w;
 		cin >> k;
