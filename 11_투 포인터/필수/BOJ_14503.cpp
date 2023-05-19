94189571396372b535929c482f77188f57fab6a8
cakenpeace <chwon02@naver.com>
2023-05-19 13:03:37 +0900

    [투 포인터] 0519 Update

--- 11_투 포인터/필수/BOJ_14503.cpp
+++ 11_투 포인터/필수/BOJ_14503.cpp
@@ -5,23 +5,10 @@ using namespace std;
 
 typedef pair<int, int> pi;
 
-//주어진 좌표의 주변 4칸이 청소되어있지 않고 비어있는지 확인하는 함수
-bool isEmpty(vector<vector<int>>& coor, pi current_cell) {
-    int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
-
-    for (int i = 0; i < 4; i++) {
-        int nx = current_cell.first + dx[i], ny = current_cell.second + dy[i];
-        if (coor[nx][ny] == 0) {
-            return true;    //청소되지 않은 칸이 있다면 true.
-        }
-    }
-    return false;   //없다면 false.
-}
-
 //로봇 청소기를 조건에 따라 이동시키고, 청소한 칸의 수를 세어 리턴하는 함수
 int countCleanedCells (vector<vector<int>>& coor, pi current_cell, int dir) {
     int cnt = 0;    //청소한 칸 수
-    int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};   
+    int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
     bool is_stop = false;   //멈추면 true.
 
     while (!is_stop) {
@@ -29,26 +16,28 @@ int countCleanedCells (vector<vector<int>>& coor, pi current_cell, int dir) {
             coor[current_cell.first][current_cell.second] = 2;
             cnt++;  //청소한 칸 수 증가
         }
-
-        if (isEmpty(coor, current_cell)) {  //만약 주위 칸 중 청소되지 않은 칸이 있을 때
-            //반시계 방향으로 회전.
+        int ori_dir = dir;
+        while (true){
+            //먼저, 반시계 방향으로 회전.
             if (dir != 0) {
                 dir--;
             } else {
                 dir = 3;
             }
             int nx = current_cell.first + dx[dir], ny = current_cell.second + dy[dir];
-            if (nx < coor.size() && ny < coor[0].size() && coor[nx][ny] == 0) {    //바라보는 방향 기준 앞 칸이 청소되지 않은 빈 칸일 때
+            if (coor[nx][ny] ==0) {    //바라보는 방향 기준 앞 칸이 청소되지 않은 빈 칸일 때
                 current_cell.first = nx; current_cell.second = ny;  //그 방향으로 전진
+                break;  //반복 종료
             }
-        }
-        else {
-            int nx = current_cell.first - dx[dir], ny = current_cell.second - dy[dir];  //바라보는 방향 기준 뒷 칸 확인
-            if (nx < 0 || ny < 0 || coor[nx][ny] == 1) {    //뒷칸이 벽일 때
-                is_stop = true;     //정지
-            }
-            else {
-                current_cell.first = nx; current_cell.second = ny;  //후진
+            if (dir == ori_dir) {   //한 바퀴를 다 돌았는데도 청소되지 않은 빈 칸이 없는 경우
+                nx = current_cell.first - dx[dir], ny = current_cell.second - dy[dir];  //바라보는 방향 기준 뒷 칸 확인
+                if (coor[nx][ny] == 1) {    //뒷칸이 벽일 때
+                    is_stop = true;     //정지
+                }
+                else {
+                    current_cell.first = nx; current_cell.second = ny;  //후진
+                }   
+                break;  //반복 종료
             }
         }
     }
