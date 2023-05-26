b29932535e1be86e96585dd31857c5a67514b748
cakenpeace <chwon02@naver.com>
2023-05-26 18:35:02 +0900

    [트리] 0526 - 추가제출

--- 12_트리/필수/BOJ_5639.cpp
+++ 12_트리/필수/BOJ_5639.cpp
@@ -1,44 +1,53 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
-int tree[10000] = {0};
+void postOrder(int left, int right, vector<int>& tree) {
+	if (left > right) {   //왼쪽 인덱스가 오른쪽보다 커졌을 때 -> 탐색 종료.
+		return;
+	}
 
-int total_idx = 0;
+	int root = tree[left];//이해하기 쉽게 루트 따로 저장해주자!
 
-void expGraph(int begin, int end) {
-  int idx = begin + 1, root = tree[begin];
+	//루트보다 처음으로 큰 노드 인덱스 찾기 -> left, right새로 설정하기 위해서!
+	int tmp = left + 1;   //left보다 1 큰 값부터 시작
+	for (int i = left+1 ; i <= right ; i++) {
+		if (tree[i] > root) {   //만약 i 값이 루트보다 클 때 -> 다른 트리
+			tmp = i;  //i를 tmp로
+			break;
+		}
+	}
 
-  if (begin == end) {
-      return;
-  }
+	//후위 순회 순으로 다시 탐색 : 왼 - 오 - 루트
+	postOrder(left + 1, tmp - 1,tree);//루트보다 작은 노드들
+	postOrder(tmp, right, tree);//루트보다 큰 노드들
 
-  while (tree[idx] < root) {
-    idx++;
-    if (idx == total_idx) {
-        return;
-    }
-  }
-  expGraph(begin + 1, idx);
-    cout << tree[idx-1] << '\n';
-    cout << root << '\n';
-  expGraph(idx, end);
-  cout << tree[idx-1] << '\n';
-  cout << root << '\n';
-
-
-
-
-
+	//left, right 탐색 끝났으므로 root 출력
+	cout << root << '\n';
 }
 
+/*
+* 이진 검색 트리 : 루트의 왼쪽 
+* 이진 검색 트리를 전위 순회환 결과 -> 후위 순회한 결과
+* 전위 순회 : 루트 왼쪽 오른쪽 -> 후위 순회 : 왼쪽 오른쪽 루트
+*  -> 탐색 결과 : 루트 -> 루트보다 작은 노드(왼쪽) -> 루트보다 큰 노드(오른쪽)
+*  -> 루트를 기준으로 left, right 나눠서 후위 순회 순으로 다시 탐색
+*/
+
 int main() {
-  int n;
 
-  while (total_idx < 9) {
-  cin >> n;
-  tree[total_idx++] = n;
-  }
-  expGraph(0, total_idx);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
+	//입력
+	int num;
+	vector<int> tree;
+	while (cin >> num) {
+		tree.push_back(num);
+	}
+
+	//연산 + 출력
+	postOrder(0, tree.size() - 1, tree);
 }
