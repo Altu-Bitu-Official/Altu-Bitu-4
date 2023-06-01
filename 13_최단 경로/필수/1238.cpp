cec1b9522f1164b8e90e39d62152cbd94b78c347
choi-yujin <124590613+choi-yujin@users.noreply.github.com>
2023-06-01 23:22:09 +0900

    [13 최단경로] 0601-추가제출

--- 13_최단 경로/필수/1238.cpp
+++ 13_최단 경로/필수/1238.cpp
@@ -6,42 +6,46 @@
 //단반향 도로이므로 가는길과 돌아오는 길을 구분함을 주의 
 //따라서 걸리는 시간을 배열로 만들어 가장 큰 값을 출력
 
-int INF = 987654321;
+int inf= 987654321;
 using namespace std;
 
-int N, M, X;
-int Dist[1010], Res[1010];
-vector<pair<int, int>> V[1010];//간선의 정보 저장
+int n, m, x;
+int dist[1010], res[1010];
+vector<pair<int, int>> v[1010];//간선의 정보 저장
 
 void Input(){
-    cin >> N >> M >> X; //초기 입력값
+    cin >> n >> m >> x; //초기 입력값
 
-    for (int i = 0; i < M; i++){
+    for (int i = 0; i < m; i++){
         int a, b, c;
         cin >> a >> b >> c;
-        V[a].push_back(make_pair(b, c));//간선의 정보를 push
+        v[a].push_back(make_pair(b, c));//간선의 정보를 push
     }
 }
 
-void Dijkstra(int Start){
+void Dijkstra(int start){
 
     priority_queue<pair<int, int>> PQ;
-    Dist[Start] = 0;
-    PQ.push(make_pair(0, Start));
+    dist[start] = 0;
+    PQ.push(make_pair(0, start));
 
     while (PQ.empty() == 0){
 
-        int Cost = -PQ.top().first;
-        int Cur = PQ.top().second;
+        int cost = -PQ.top().first;
+        int cur = PQ.top().second;
         PQ.pop(); //coast와 cur에 저장한 다음 pop
 
-        for (int i = 0; i < V[Cur].size(); i++){
-            int Next = V[Cur][i].first;
-            int nCost = V[Cur][i].second;
+        if (cost > dist[cur]) {//탐색하려는 값이 기존 값 보다 크다면 탐색할 필요x->구하지 않고 넘어감
+            continue;
+        }
 
-            if (Dist[Next] > Cost + nCost){//만약 더 크다면->저장
-                Dist[Next] = Cost + nCost;
-                PQ.push(make_pair(-Dist[Next], Next));
+        for (int i = 0; i < v[cur].size(); i++){
+            int next = v[cur][i].first;
+            int nCost = v[cur][i].second;
+
+            if (dist[next] > cost + nCost){//만약 더 크다면->최소값으로 갱신->pq에 저장
+                dist[next] = cost + nCost;
+                PQ.push(make_pair(-dist[next], next));
             }
         }
     }
@@ -50,28 +54,27 @@ void Dijkstra(int Start){
 
 void Solution() {
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            Dist[j] = INF;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            dist[j] = inf;
         }
         Dijkstra(i);
-        Res[i] = Dist[X];
+        res[i] = dist[x];//i에서 x까지 가는데 걸리는 시간을 구함->res[i]에 저장
     }
-    for (int j = 1; j <= N; j++) {
-        Dist[j] = INF;
-    }
-
-    Dijkstra(X);
-    for (int i = 1; i <= N; i++) {
-        Res[i] = Res[i] + Dist[i];
+    for (int j = 1; j <= n; j++) {
+        dist[j] = inf;//배열 다시 초기화
     }
 
-    sort(Res + 1, Res + N + 1);// 가장 큰 값을 찾기 위해 정렬
-   int  Answer = Res[N];//가장 큰 값
+    Dijkstra(x);// x에서 i까지 가는데 걸리는 시간
+    for (int i = 1; i <= n; i++) {
+        res[i] = res[i] + dist[i];//총 소요 시간=가는데 걸리는 시간+오는데 걸리는 시간 (도로가 단방향이므로)
+    }
+
+    sort(res + 1, res + n + 1);// 가장 큰 값을 찾기 위해 정렬
+   int  answer = res[n];//가장 큰 값
     
-    cout << Answer;
+    cout << answer;
 }
-
 
 int main(){
     ios::sync_with_stdio(false);
