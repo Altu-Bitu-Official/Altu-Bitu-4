5cedeff149844c72c2625f20dd1e2f8fdf92e71a
sforseohn <sforseohn@ewhain.net>
2023-06-02 16:35:53 +0900

    [최단 경로] 추가제출

---
+++ 13_최단 경로/필수/15685.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int SIZE = 100; // 평면 가로, 세로 길이의 최댓값
+
+// 방향: 우(0), 상(1), 좌(2), 하(3)
+int dy[4] = { 0, -1, 0, 1 }; // 상하 방향
+int dx[4] = { 1, 0, -1, 0 }; // 좌우 방향
+
+// 1x1 정사각형 개수 계산
+int cntSquares(vector<vector<bool>>& plane) {
+	int ans = 0; //  ans 0으로 초기화
+	for (int i = 0; i < SIZE; i++) {     // 최대 y 길이만큼
+		for (int j = 0; j < SIZE; j++) { // 최대 x 길이만큼
+
+		    // 정사각형의 네 꼭짓점이 모두 1이면
+			if (plane[i][j] && plane[i + 1][j] && plane[i][j + 1] && plane[i + 1][j + 1]) {
+				ans++; // ans 증가
+			}
+		}
+	}
+	return ans; // ans 반환
+}
+
+// 평면에 드래곤 커브를 표시
+void drawDragonCurve(vector<vector<bool>>& plane, int x, int y, int d, int g) {
+	vector<int> direct; // 방향 저장
+	plane[y][x] = plane[y + dy[d]][x + dx[d]] = true; // 평면에 표시 (시작 방향대로 초기화)
+	x += dx[d]; // 시작 방향대로 x좌표 업데이트
+	y += dy[d]; // 시작 방향대로 y좌표 업데이트
+	direct.push_back(d); // direct에 d 추가
+	while (g--) { // 1 ~ g 세대
+		int size_d = direct.size(); // 방향의 size
+		for (int j = size_d - 1; j >= 0; j--) { // 방향 계산
+			int next_d = (direct[j] + 1) % 4; // 다음 방향
+			x += dx[next_d]; // 다음 방향대로 x좌표 업데이트
+			y += dy[next_d]; // 다음 방향대로 y좌표 업데이트
+			plane[y][x] = true; // 평면에 표시
+			direct.push_back(next_d); // direct에 다음 방향 추가
+		}
+	}
+}
+
+/*
+* 규칙
+* 0 세대: 0
+* 1 세대: 0 1
+* 2 세대: 0 1 2 1
+* 3 세대: 0 1 2 1 2 3 2 1
+* ...
+* N 세대: concat((N-1세대), ((N-1세대 거꾸로) + 1)%4)
+* 평면(좌측 상단이 (0, 0))에 드래곤 커브를 그린 후 정사각형의 개수를 계산
+* 드래곤 커브는 평면 밖으로 나가지 않음으로 범위를 확인할 필요 없음
+* 1. 0 세대의 드래곤 커브를 먼저 저장 (초기 조건)
+* 2. 세대를 거듭하면서 드래곤 커브를 그림 (규칙을 파악하는 것이 중요)
+* 3. 드래곤 커브가 그려진 평면 상의 정사각형의 개수 계산 (네 꼭짓점 확인)
+*/
+
+int main()
+{
+	int n, x, y, d, g;
+	vector<vector<bool>> plane(SIZE + 1, vector<bool>(SIZE + 1, false)); // 평면
+
+	// 입력
+	cin >> n; // 정사각형의 네 꼭짓점이 모두 드래곤 커브의 일부인 것의 개수
+
+	// 연산
+	while (n--) { // n번 동안
+		cin >> x >> y >> d >> g; // 드래곤 커브의 시작점, 시작 방향, 세대
+		drawDragonCurve(plane, x, y, d, g); // 드래곤 커브 그리기
+	}
+
+	// 출력
+	cout << cntSquares(plane) << '\n'; // 1x1 정사각형 개수 계산
+
+	return 0;
+}
